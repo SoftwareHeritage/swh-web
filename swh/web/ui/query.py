3313021a68ad6d0daa1bd2fe2266aec39bcc2255
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-24 14:46:17 +0200

    Improve checks against bad input

--- swh/web/ui/query.py
+++ swh/web/ui/query.py
@@ -12,11 +12,14 @@ from swh.core import hashutil
 def parse(query):
     """Parse a formalized get query.
 
-    Arg:
+    Args:
         query, a colon separated value hash
 
     Returns:
         List of hashes
+
+    Raises:
+        AttributeError if query is None
 
     """
     return query.split(':')
@@ -29,12 +32,26 @@ sha1_regexp='[0-9a-f]{40}'
 
 def group_by_checksums(hashes):
     """Check that the checksums have the right format.
+
+    Args:
+        hashes: a list of string which should represent hashes (sha1 or sha256)
+
+    Returns:
+        A dictionary of hash indexed by their nature (sha1, sha256)
+        The dictionary will be empty if nothing matches
+
+    Raises:
+        None
+
     """
     hashes_m = {}
     for h in hashes:
-        if re.search(sha256_regexp, h):
-            hashes_m.update({'sha256': hashutil.hex_to_hash(h)})
-        elif re.search(sha1_regexp, h):
-            hashes_m.update({'sha1': hashutil.hex_to_hash(h)})
+        try:
+            if re.search(sha256_regexp, h):
+                hashes_m.update({'sha256': hashutil.hex_to_hash(h)})
+            elif re.search(sha1_regexp, h):
+                hashes_m.update({'sha1': hashutil.hex_to_hash(h)})
+        except ValueError:  # ignore silently to check the other inputs
+            continue
 
     return hashes_m
--- swh/web/ui/tests/test_query.py
+++ swh/web/ui/tests/test_query.py
@@ -21,6 +21,12 @@ class QueryTestCase(unittest.TestCase):
                           "Should be a, b, c hashes")
 
     @istest
+    def parse_with_error(self):
+        # when
+        with self.assertRaises(AttributeError):
+            query.parse(None)
+
+    @istest
     def parse_2(self):
         # when
         actual_hashes = query.parse("a")
@@ -32,11 +38,27 @@ class QueryTestCase(unittest.TestCase):
     @istest
     def group_by_checksum(self):
         input_sha1 = 'f1d2d2f924e986ac86fdf7b36c94bcdf32beec15'
-        input_sha256 = '084c799cd551dd1d8d5c5f9a5d593b2e931f5e36122ee5c793c1d08a19839cc0'
+        input_sha256 = \
+            '084c799cd551dd1d8d5c5f9a5d593b2e931f5e36122ee5c793c1d08a19839cc0'
+        input_bad_length = '1234567890987654'
 
-        res = query.group_by_checksums([input_sha1, input_sha256, input_sha1])
+        res = query.group_by_checksums([input_sha1,
+                                        input_sha256,
+                                        input_bad_length,
+                                        input_sha1])
 
-        # for i in res:
-        #     print(i)
+        self.assertEquals(len(res), 2,
+                          "Only 2 elements because no duplicates and filter out bad inputs.")
         self.assertEquals(res['sha1'], hashutil.hex_to_hash(input_sha1))
         self.assertEquals(res['sha256'], hashutil.hex_to_hash(input_sha256))
+
+    @istest
+    def group_by_checksum_with_only_bad_inputs(self):
+        input_sha1 = 'e986ac86fdf7b36c94bcdf32beec15'
+        input_bad_length = '1234567890987654'
+
+        res = query.group_by_checksums([input_sha1,
+                                        input_bad_length,
+                                        input_sha1])
+
+        self.assertEquals(res, {}, "Absolutely no good inputs.")
