556ed987438e26b5b21eaec71ff508e27df66c7d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-29 09:40:50 +0200

    Refactor - Migrate to content_find

--- swh/web/ui/service.py
+++ swh/web/ui/service.py
@@ -22,7 +22,7 @@ def lookup_hash(q):
     """
     hash = query.categorize_hash(q)
     if hash != {}:
-        present = main.storage().content_present(hash)
+        present = main.storage().content_find(hash)
         return 'Found!' if present else 'Not Found'
     return """This is not a hash.
 Hint: hexadecimal string with length either 20 (sha1) or 32 (sha256)."""
