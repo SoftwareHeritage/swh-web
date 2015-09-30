2cade8d56c4f8cb113e69938ee427329d22c68cf
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-30 16:33:39 +0200

    Add package data

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+def parse_requirements():
+    requirements = []
+    with open('requirements.txt') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
+
+    return requirements
+
+
+setup(
+    name='swh.web.ui',
+    description='Software Heritage Web UI',
+    author='Software Heritage developers',
+    author_email='swh-devel@inria.fr',
+    url='https://forge.softwareheritage.org/diffusion/DWUI/',
+    packages=['swh.web.ui', 'swh.web.ui.tests'],
+    scripts=['bin/swh-web-ui'],
+    install_requires=parse_requirements(),
+    setup_requires=['vcversioner'],
+    vcversioner={},
+    include_package_data=True,
+)
