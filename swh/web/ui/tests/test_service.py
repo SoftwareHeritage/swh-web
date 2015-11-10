497326fbf7c4e893783527f461e0ac22400d629b
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2015-11-10 10:51:42 +0100

    Mock the storage using unittest.mock
    Improve test readability and maintenance.
    
    The setup needs some clarification though

--- swh/web/ui/tests/test_service.py
+++ swh/web/ui/tests/test_service.py
@@ -7,15 +7,83 @@ import unittest
 
 from nose.tools import istest
 
-from unittest.mock import patch
+from unittest.mock import MagicMock
 
-from swh.web.ui import service
-from swh.web.ui import controller
+from swh.storage.api.client import RemoteStorage as Storage
+from swh.web.ui import service, controller
 
 
-class MockStorage():
+# Because the Storage's __init__ function does side effect at startup...
+class RemoteStorageAdapter(Storage):
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+
+class ServiceTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.storage = RemoteStorageAdapter('http://localhost:5000/')  # Mock
+
+        # inject the mock data
+        conf = {'api_backend': 'https://somewhere.org:4321',
+                'storage': cls.storage}
+
+        controller.app.config['TESTING'] = True
+        controller.app.config.update({'conf': conf})
+        cls.app = controller.app.test_client()
+
+    @istest
+    def lookup_hash_does_not_exist(self):
+        # given
+        self.storage.content_exist = MagicMock(return_value=False)
+
+        # when
+        actual_lookup = service.lookup_hash(
+            'sha1:123caf10e9535160d90e874b45aa426de762f19f')
+
+        self.assertFalse(actual_lookup)
+
+    @istest
+    def lookup_hash_exist(self):
+        # given
+        self.storage.content_exist = MagicMock(return_value=True)
+
+        # when
+        actual_lookup = service.lookup_hash(
+            'sha1:123caf10e9535160d90e874b45aa426de762f19f')
+
+        self.assertTrue(actual_lookup)
+
+    @istest
+    def lookup_hash_origin(self):
+        # given
+        self.storage.content_find_occurrence = MagicMock(return_value={
+            'origin_type': 'sftp',
+            'origin_url': 'sftp://ftp.gnu.org/gnu/octave',
+            'branch': 'octavio-3.4.0.tar.gz',
+            'revision': b'\xb0L\xaf\x10\xe9SQ`\xd9\x0e\x87KE\xaaBm\xe7b\xf1\x9f',  # noqa
+            'path': b'octavio-3.4.0/doc/interpreter/octave.html/doc_002dS_005fISREG.html'  # noqa
+        })
+        expected_origin = {
+            'origin_type': 'sftp',
+            'origin_url': 'sftp://ftp.gnu.org/gnu/octave',
+            'branch': 'octavio-3.4.0.tar.gz',
+            'revision': 'b04caf10e9535160d90e874b45aa426de762f19f',
+            'path': 'octavio-3.4.0/doc/interpreter/octave.html/doc'
+                    '_002dS_005fISREG.html'
+        }
+
+        # when
+        actual_origin = service.lookup_hash_origin(
+            'sha1_git:456caf10e9535160d90e874b45aa426de762f19f')
+
+        # then
+        self.assertEqual(actual_origin, expected_origin)
+
+    @istest
     def stat_counters(self):
-        return {
+        # given
+        input_stats = {
             "content": 1770830,
             "directory": 211683,
             "directory_entry_dir": 209167,
@@ -32,69 +100,11 @@ class MockStorage():
             "revision_history": 0,
             "skipped_content": 0
         }
-
-    def content_find_occurrence(self, h):
-        return {
-            'origin_type': 'sftp',
-            'origin_url': 'sftp://ftp.gnu.org/gnu/octave',
-            'branch': 'octavio-3.4.0.tar.gz',
-            'revision': b'\xb0L\xaf\x10\xe9SQ`\xd9\x0e\x87KE\xaaBm\xe7b\xf1\x9f',  # noqa
-            'path': b'octavio-3.4.0/doc/interpreter/octave.html/doc_002dS_005fISREG.html'  # noqa
-        }
-
-    def content_exist(self, h):
-        return False
-
-
-class ServiceTestCase(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # inject the mock data
-        conf = {'api_backend': 'https://somewhere.org:4321',
-                'storage': MockStorage()}
-
-        controller.app.config['TESTING'] = True
-        controller.app.config.update({'conf': conf})
-        cls.app = controller.app.test_client()
-
-    @istest
-    def lookup_hash(self):
-        with patch('swh.web.ui.main.storage',
-                   return_value=MockStorage()):
-            actual_lookup = service.lookup_hash(
-                'sha1:123caf10e9535160d90e874b45aa426de762f19f')
-
-        self.assertFalse(actual_lookup)
-
-    @istest
-    def lookup_hash_origin(self):
-        # given
-        expected_origin = {
-            'origin_type': 'sftp',
-            'origin_url': 'sftp://ftp.gnu.org/gnu/octave',
-            'branch': 'octavio-3.4.0.tar.gz',
-            'revision': 'b04caf10e9535160d90e874b45aa426de762f19f',
-            'path': 'octavio-3.4.0/doc/interpreter/octave.html/doc'
-                    '_002dS_005fISREG.html'
-        }
+        self.storage.stat_counters = MagicMock(return_value=input_stats)
 
         # when
-        # with patch('swh.web.ui.main.storage',
-        #            return_value=MockStorage()):
-        actual_origin = service.lookup_hash_origin(
-            'sha1_git:456caf10e9535160d90e874b45aa426de762f19f')
-
-        # then
-        self.assertEqual(actual_origin, expected_origin)
-
-    @istest
-    def stat_counters(self):
-        # given
-        expected_stats = MockStorage().stat_counters()
-
-        # when
-        # with patch('swh.web.ui.main.storage', return_value=MockStorage()):
         actual_stats = service.stat_counters()
 
         # then
+        expected_stats = input_stats
         self.assertEqual(actual_stats, expected_stats)
