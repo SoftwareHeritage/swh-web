099dbf5fb5ec3123f0a58b2f64d8013c7542c8fd
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2015-12-01 15:00:28 +0100

    Separate api test from views test

--- swh/web/ui/tests/test_api.py
+++ swh/web/ui/tests/test_api.py
@@ -19,32 +19,6 @@ class ApiTestCase(unittest.TestCase):
     def setUpClass(cls):
         cls.app, _, _ = test_app.init_app()
 
-    @istest
-    def info(self):
-        # when
-        rv = self.app.get('/about')
-
-        self.assertEquals(rv.status_code, 200)
-        self.assertIn(b'About', rv.data)
-
-    # @istest
-    def search_1(self):
-        # when
-        rv = self.app.get('/search')
-
-        self.assertEquals(rv.status_code, 200)  # check this api
-        self.assertRegexpMatches(rv.data, b'name=q value=>')
-
-    # @istest
-    def search_2(self):
-        # when
-        rv = self.app.get('/search?q=one-hash-to-look-for:another-one')
-
-        self.assertEquals(rv.status_code, 200)  # check this api
-        self.assertRegexpMatches(
-            rv.data,
-            b'name=q value=one-hash-to-look-for:another-one')
-
     @patch('swh.web.ui.api.service')
     @istest
     def api_content_checksum_to_origin(self, mock_service):
---
+++ swh/web/ui/tests/test_views.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2015  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU Affero General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import unittest
+
+from nose.tools import istest
+
+from swh.web.ui.tests import test_app
+
+
+class ViewsTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.app, _, _ = test_app.init_app()
+
+    @istest
+    def info(self):
+        # when
+        rv = self.app.get('/about')
+
+        self.assertEquals(rv.status_code, 200)
+        self.assertIn(b'About', rv.data)
+
+    # @istest
+    def search_1(self):
+        # when
+        rv = self.app.get('/search')
+
+        self.assertEquals(rv.status_code, 200)  # check this api
+        self.assertRegexpMatches(rv.data, b'name=q value=>')
+
+    # @istest
+    def search_2(self):
+        # when
+        rv = self.app.get('/search?q=one-hash-to-look-for:another-one')
+
+        self.assertEquals(rv.status_code, 200)  # check this api
+        self.assertRegexpMatches(
+            rv.data,
+            b'name=q value=one-hash-to-look-for:another-one')
