8ac2238e7097d9d030b957eacf39dbab1dabe387
Jordi BERTRAN DE BALANDA (swhintern) <j.debalanda@gmail.com>
2016-08-23 12:53:08 +0200

    renderers: use inspect module instead of trim fn

--- swh/web/ui/renderers.py
+++ swh/web/ui/renderers.py
@@ -6,10 +6,10 @@
 import re
 import yaml
 import json
-import sys
 
 from docutils.core import publish_parts
 from docutils.writers.html4css1 import Writer, HTMLTranslator
+from inspect import cleandoc
 
 from flask import request, Response, render_template
 from flask import g
@@ -136,30 +136,7 @@ def safe_docstring_display(docstring):
     api.
     """
 
-    def trim(docstring):
-        """Correctly trim triple-quoted docstrings, taking into account
-        first-line indentation inconsistency.
-        Source: https://www.python.org/dev/peps/pep-0257/#handling-docstring-indentation  # noqa
-        """
-        if not docstring:
-            return ''
-        lines = docstring.expandtabs().splitlines()
-        indent = sys.maxsize
-        for line in lines[1:]:
-            stripped = line.lstrip()
-            if stripped:
-                indent = min(indent, len(line) - len(stripped))
-        trimmed = [lines[0].strip()]
-        if indent < sys.maxsize:
-            for line in lines[1:]:
-                trimmed.append(line[indent:].rstrip())
-        while trimmed and not trimmed[-1]:
-            trimmed.pop()
-        while trimmed and not trimmed[0]:
-            trimmed.pop(0)
-        return '\n'.join(trimmed)
-
-    docstring = trim(docstring)
+    docstring = cleandoc(docstring)
     return publish_parts(docstring, writer=DOCSTRING_WRITER)['html_body']
 
 
