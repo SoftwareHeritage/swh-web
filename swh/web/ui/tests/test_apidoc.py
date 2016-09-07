3e926e989954f6e50ecb3931522792d20088efc0
Jordi BERTRAN DE BALANDA (swhintern) <j.debalanda@gmail.com>
2016-09-07 15:03:47 +0200

    apidoc: harden the decorator stack
      - Only requirement is now to have @apidoc.route at the top of the
        tower
      - Tests are now integration tests
      - Update docstrings and README-dev for more accurate description
    
    Related T607

--- swh/web/ui/tests/test_apidoc.py
+++ swh/web/ui/tests/test_apidoc.py
@@ -4,16 +4,17 @@
 # See top-level LICENSE file for more information
 
 
-from unittest.mock import MagicMock, patch
-from nose.tools import istest
+from nose.tools import istest, nottest
 
 from swh.web.ui import apidoc
-from swh.web.ui.tests import test_app
+from swh.web.ui.main import app
+from swh.web.ui.tests.test_app import SWHApidocTestCase
 
 
-class APIDocTestCase(test_app.SWHApidocTestCase):
+class APIDocTestCase(SWHApidocTestCase):
 
     def setUp(self):
+
         self.arg_dict = {
             'name': 'my_pretty_arg',
             'default': 'some default value',
@@ -37,310 +38,88 @@ class APIDocTestCase(test_app.SWHApidocTestCase):
             'doc': 'a dict with amazing properties'
         }
 
-    @patch('swh.web.ui.apidoc.APIUrls')
-    @patch('swh.web.ui.apidoc.app')
-    @istest
-    def apidoc_route(self, mock_app, mock_api_urls):
-        # given
-        decorator = apidoc.route('/some/url/for/doc/')
-        mock_fun = MagicMock(return_value=123)
-        mock_fun.__doc__ = 'Some documentation'
-        mock_fun.__name__ = 'some_fname'
-        decorated = decorator.__call__(mock_fun)
-
-        # when
-        decorated('some', 'value', kws='and a kw')
-
-        # then
-        mock_fun.assert_called_once_with(
-            call_args=(('some', 'value'), {'kws': 'and a kw'}),
-            doc_route='/some/url/for/doc/',
-            noargs=False
-        )
-        mock_api_urls.index_add_route.assert_called_once_with(
-            '/some/url/for/doc/',
-            'Some documentation')
-        mock_app.add_url_rule.assert_called_once_with(
-            '/some/url/for/doc/', 'some_fname', decorated)
+    @apidoc.route('/my/nodoc/url/')
+    @nottest
+    def apidoc_nodoc_tester(arga, argb):
+        return arga + argb
 
     @istest
-    def apidoc_arg_noprevious(self):
-        # given
-        decorator = apidoc.arg('my_pretty_arg',
-                               default='some default value',
-                               argtype=apidoc.argtypes.sha1,
-                               argdoc='this arg does things')
-        mock_fun = MagicMock(return_value=123)
-        decorated = decorator.__call__(mock_fun)
-        self.arg_dict['type'] = self.arg_dict['type'].value
-
-        # when
-        decorated(call_args=((), {}), doc_route='some/route/')
-
-        # then
-        mock_fun.assert_called_once_with(
-            call_args=((), {}),
-            doc_route='some/route/',
-            args=[self.arg_dict]
-        )
+    def apidoc_nodoc_failure(self):
+        with self.assertRaises(Exception):
+            self.client.get('/my/nodoc/url/')
 
     @istest
-    def apidoc_arg_previous(self):
-        # given
-        decorator = apidoc.arg('my_other_arg',
-                               default='some other value',
-                               argtype=apidoc.argtypes.sha1,
-                               argdoc='this arg is optional')
-        mock_fun = MagicMock(return_value=123)
-        decorated = decorator.__call__(mock_fun)
+    def apidoc_badorder_failure(self):
+        with self.assertRaises(AssertionError):
+            @app.route('/my/badorder/url/<int:foo>/')
+            @apidoc.arg('foo',
+                        default=True,
+                        argtype=apidoc.argtypes.int,
+                        argdoc='It\'s so fluffy!')
+            @apidoc.route('/my/badorder/url/')
+            @nottest
+            def apidoc_badorder_tester(foo, bar=0):
+                """
+                Some irrelevant doc since the decorators are bad
+                """
+                return foo + bar
 
-        # when
-        decorated(call_args=((), {}),
-                  doc_route='some/route/',
-                  args=[self.arg_dict])
-
-        # then
-        mock_fun.assert_called_once_with(
-            call_args=((), {}),
-            doc_route='some/route/',
-            args=[self.arg_dict,
-                  {'name': 'my_other_arg',
-                   'default': 'some other value',
-                   'type': apidoc.argtypes.sha1.value,
-                   'doc': 'this arg is optional'}])
-
-    @istest
-    def apidoc_raises_noprevious(self):
-        # given
-        decorator = apidoc.raises(exc=apidoc.excs.badinput,
-                                  doc='My exception documentation')
-        mock_fun = MagicMock(return_value=123)
-        decorated = decorator.__call__(mock_fun)
-        self.stub_excs[0]['exc'] = self.stub_excs[0]['exc'].value
-
-        # when
-        decorated(call_args=((), {}), doc_route='some/route/')
-
-        # then
-        mock_fun.assert_called_once_with(
-            call_args=((), {}),
-            doc_route='some/route/',
-            excs=self.stub_excs
-        )
+    @app.route('/some/<int:myarg>/<int:myotherarg>/')
+    @apidoc.route('/some/doc/route/')
+    @nottest
+    def apidoc_route_tester(myarg, myotherarg, akw=0):
+        """
+        Sample doc
+        """
+        return {'result': myarg + myotherarg + akw}
 
     @istest
-    def apidoc_raises_previous(self):
-        # given
-        decorator = apidoc.raises(exc=apidoc.excs.notfound,
-                                  doc='Another documentation')
-        mock_fun = MagicMock(return_value=123)
-        decorated = decorator.__call__(mock_fun)
-        expected_excs = self.stub_excs + [{
-            'exc': apidoc.excs.notfound.value,
-            'doc': 'Another documentation'}]
-        expected_excs[0]['exc'] = expected_excs[0]['exc'].value
-
+    def apidoc_route_doc(self):
         # when
-        decorated(call_args=((), {}),
-                  doc_route='some/route/',
-                  excs=self.stub_excs)
+        rv = self.client.get('/some/doc/route/')
 
         # then
-        mock_fun.assert_called_once_with(
-            call_args=((), {}),
-            doc_route='some/route/',
-            excs=expected_excs)
+        self.assertEqual(rv.status_code, 200)
+        self.assert_template_used('apidoc.html')
 
-    @patch('swh.web.ui.apidoc.render_template')
-    @patch('swh.web.ui.apidoc.url_for')
-    @patch('swh.web.ui.apidoc.APIUrls')
-    @patch('swh.web.ui.apidoc.request')
     @istest
-    def apidoc_returns_doc_call(self,
-                                mock_request,
-                                mock_api_urls,
-                                mock_url_for,
-                                mock_render):
-        # given
-        decorator = apidoc.returns(rettype=apidoc.rettypes.dict,
-                                   retdoc='a dict with amazing properties')
-        mock_fun = MagicMock(return_value=123)
-        mock_fun.__name__ = 'some_fname'
-        mock_fun.__doc__ = 'Some documentation'
-        decorated = decorator.__call__(mock_fun)
-
-        mock_api_urls.get_method_endpoints.return_value = self.stub_rule_list
-
-        mock_request.url = 'http://my-domain.tld/some/doc/route/'
-        mock_request.method = 'GET'
-        mock_url_for.return_value = 'http://my-domain.tld/meaningful_route/'
-
-        expected_env = {
-            'urls': [{'rule': 'some/route/with/args/',
-                      'methods': {'GET', 'HEAD', 'OPTIONS'}},
-                     {'rule': 'some/other/route/',
-                      'methods': {'GET', 'HEAD', 'OPTIONS'}}],
-            'docstring': 'Some documentation',
-            'args': self.stub_args,
-            'excs': self.stub_excs,
-            'route': 'some/doc/route/',
-            'example': 'http://my-domain.tld/meaningful_route/',
-            'return': self.stub_return
-        }
+    def apidoc_route_fn(self):
 
         # when
-        decorated(
-            docstring='Some documentation',
-            call_args=(('some', 'args'), {'kw': 'kwargs'}),
-            args=self.stub_args,
-            excs=self.stub_excs,
-            doc_route='some/doc/route/',
-            noargs=False
-        )
+        rv = self.client.get('/some/1/1/')
 
         # then
-        self.assertEqual(mock_fun.call_args_list, [])  # function not called
-        mock_render.assert_called_once_with(
-            'apidoc.html',
-            **expected_env
-        )
+        self.assertEqual(rv.status_code, 200)
 
-    @patch('swh.web.ui.apidoc.g')
-    @patch('swh.web.ui.apidoc.url_for')
-    @patch('swh.web.ui.apidoc.APIUrls')
-    @patch('swh.web.ui.apidoc.request')
+    @app.route('/some/full/<int:myarg>/<int:myotherarg>/')
+    @apidoc.route('/some/complete/doc/route/')
+    @apidoc.arg('myarg',
+                default=67,
+                argtype=apidoc.argtypes.int,
+                argdoc='my arg')
+    @apidoc.raises(exc=apidoc.excs.badinput, doc='Oops')
+    @apidoc.returns(rettype=apidoc.rettypes.dict,
+                    retdoc='sum of args')
+    @nottest
+    def apidoc_full_stack_tester(myarg, myotherarg, akw=0):
+        """
+        Sample doc
+        """
+        return {'result': myarg + myotherarg + akw}
+
     @istest
-    def apidoc_returns_noargs(self,
-                              mock_request,
-                              mock_api_urls,
-                              mock_url_for,
-                              mock_g):
-
-        # given
-        decorator = apidoc.returns(rettype=apidoc.rettypes.dict,
-                                   retdoc='a dict with amazing properties')
-        mock_fun = MagicMock(return_value=123)
-        mock_fun.__name__ = 'some_fname'
-        mock_fun.__doc__ = 'Some documentation'
-        decorated = decorator.__call__(mock_fun)
-
-        mock_api_urls.get_method_endpoints.return_value = [
-            {'rule': 'some/doc/route/',
-             'methods': {'GET', 'HEAD', 'OPTIONS'}}]
-        mock_request.url = 'http://my-domain.tld/some/doc/route/'
-        doc_dict = {
-            'urls': [
-                {'rule': 'some/doc/route/',
-                 'methods': {'GET', 'HEAD', 'OPTIONS'}}],
-            'docstring': 'Some documentation',
-            'route': 'some/doc/route/',
-            'return': {'type': apidoc.rettypes.dict.value,
-                       'doc': 'a dict with amazing properties'}
-        }
-
+    def apidoc_full_stack_doc(self):
         # when
-        decorated(
-            call_args=((), {}),
-            doc_route='some/doc/route/',
-            noargs=True
-        )
+        rv = self.client.get('/some/complete/doc/route/')
 
         # then
-        mock_fun.assert_called_once_with()
-        self.assertEqual(mock_g.doc_env, doc_dict)
+        self.assertEqual(rv.status_code, 200)
+        self.assert_template_used('apidoc.html')
 
-    @patch('swh.web.ui.apidoc.g')
-    @patch('swh.web.ui.apidoc.url_for')
-    @patch('swh.web.ui.apidoc.APIUrls')
-    @patch('swh.web.ui.apidoc.request')
     @istest
-    def apidoc_returns_same_fun(self,
-                                mock_request,
-                                mock_api_urls,
-                                mock_url_for,
-                                mock_g):
-
-        # given
-        decorator = apidoc.returns(rettype=apidoc.rettypes.dict,
-                                   retdoc='a dict with amazing properties')
-        mock_fun = MagicMock(return_value=123)
-        mock_fun.__name__ = 'some_fname'
-        mock_fun.__doc__ = 'Some documentation'
-        decorated = decorator.__call__(mock_fun)
-
-        mock_api_urls.get_method_endpoints.return_value = [
-            {'rule': 'some/doc/route/',
-             'methods': {'GET', 'HEAD', 'OPTIONS'}},
-            {'rule': 'some/doc/route/',
-             'methods': {'POST'}}]
-        mock_request.url = 'http://my-domain.tld/some/doc/route/'
-        mock_request.method = 'POST'
-        doc_dict = {
-            'urls': [{'rule': 'some/doc/route/',
-                      'methods': {'POST'}}],
-            'docstring': 'Some documentation',
-            'route': 'some/doc/route/',
-            'return': {'type': apidoc.rettypes.dict.value,
-                       'doc': 'a dict with amazing properties'}
-        }
-
+    def apidoc_full_stack_fn(self):
         # when
-        decorated(
-            call_args=(('my', 'args'), {'kw': 'andkwargs'}),
-            doc_route='some/doc/route/',
-            noargs=False
-        )
+        rv = self.client.get('/some/full/1/1/')
 
         # then
-        mock_fun.assert_called_once_with('my', 'args', kw='andkwargs')
-        self.assertEqual(mock_g.doc_env, doc_dict)
-
-    @patch('swh.web.ui.apidoc.g')
-    @patch('swh.web.ui.apidoc.url_for')
-    @patch('swh.web.ui.apidoc.APIUrls')
-    @patch('swh.web.ui.apidoc.request')
-    @istest
-    def apidoc_return_endpoint_call(self,
-                                    mock_request,
-                                    mock_api_urls,
-                                    mock_url_for,
-                                    mock_g):
-        # given
-        decorator = apidoc.returns(rettype=apidoc.rettypes.dict,
-                                   retdoc='a dict with amazing properties')
-        mock_fun = MagicMock(return_value=123)
-        mock_fun.__name__ = 'some_fname'
-        mock_fun.__doc__ = 'Some documentation'
-        decorated = decorator.__call__(mock_fun)
-
-        mock_api_urls.get_method_endpoints.return_value = self.stub_rule_list
-
-        mock_request.url = 'http://my-domain.tld/some/arg/route/'
-        mock_url_for.return_value = 'http://my-domain.tld/some/arg/route'
-
-        doc_dict = {
-            'urls': [{'rule': 'some/route/with/args/',
-                      'methods': {'GET', 'HEAD', 'OPTIONS'}},
-                     {'rule': 'some/other/route/',
-                      'methods': {'GET', 'HEAD', 'OPTIONS'}}],
-            'docstring': 'Some documentation',
-            'args': self.stub_args,
-            'excs': self.stub_excs,
-            'route': 'some/doc/route/',
-            'example': 'http://my-domain.tld/some/arg/route',
-            'return': self.stub_return
-        }
-
-        # when
-        decorated(
-            docstring='Some documentation',
-            call_args=(('some', 'args'), {'kw': 'kwargs'}),
-            args=self.stub_args,
-            excs=self.stub_excs,
-            noargs=False,
-            doc_route='some/doc/route/',
-        )
-
-        # then
-        mock_fun.assert_called_once_with('some', 'args', kw='kwargs')
-        self.assertEqual(mock_g.doc_env, doc_dict)
+        self.assertEqual(rv.status_code, 200)
