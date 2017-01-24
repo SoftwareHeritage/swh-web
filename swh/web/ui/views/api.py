17a3f0f044feb8840b83f5c6802235c94a88a7d6
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-01-24 11:59:55 +0100

    apidoc: Hide /api/1/revision/prev/ endpoint
    Implementation wise, we separate the two endpoints and hide the one we
    will not open.
    
    Closes T643

--- swh/web/ui/views/api.py
+++ swh/web/ui/views/api.py
@@ -568,9 +568,8 @@ def api_revision_with_origin(origin_id,
         ts)
 
 
-@app.route('/api/1/revision/<string:sha1_git>/')
 @app.route('/api/1/revision/<string:sha1_git>/prev/<path:context>/')
-@doc.route('/api/1/revision/')
+@doc.route('/api/1/revision/prev/', tags=['hidden'])
 @doc.arg('sha1_git',
          default='ec72c666fb345ea5f21359b7bc063710ce558e39',
          argtype=doc.argtypes.sha1_git,
@@ -585,7 +584,7 @@ def api_revision_with_origin(origin_id,
             doc='Raised if a revision matching sha1_git was not found in SWH')
 @doc.returns(rettype=doc.rettypes.dict,
              retdoc='The metadata of the revision identified by sha1_git')
-def api_revision(sha1_git, context=None):
+def api_revision_with_context(sha1_git, context):
     """Return information about revision with id sha1_git.
     """
     def _enrich_revision(revision, context=context):
@@ -596,6 +595,28 @@ def api_revision(sha1_git, context=None):
         service.lookup_revision,
         'Revision with sha1_git %s not found.' % sha1_git,
         _enrich_revision)
+
+
+@app.route('/api/1/revision/<string:sha1_git>/')
+@doc.route('/api/1/revision/')
+@doc.arg('sha1_git',
+         default='ec72c666fb345ea5f21359b7bc063710ce558e39',
+         argtype=doc.argtypes.sha1_git,
+         argdoc="The revision's sha1_git identifier")
+@doc.raises(exc=doc.excs.badinput,
+            doc='Raised if sha1_git is not well formed')
+@doc.raises(exc=doc.excs.notfound,
+            doc='Raised if a revision matching sha1_git was not found in SWH')
+@doc.returns(rettype=doc.rettypes.dict,
+             retdoc='The metadata of the revision identified by sha1_git')
+def api_revision(sha1_git):
+    """Return information about revision with id sha1_git.
+    """
+    return _api_lookup(
+        sha1_git,
+        service.lookup_revision,
+        'Revision with sha1_git %s not found.' % sha1_git,
+        utils.enrich_revision)
 
 
 @app.route('/api/1/revision/<string:sha1_git>/raw/')
@@ -672,8 +693,8 @@ previously visited).  If multiple values, use / as delimiter.  """)
             doc="""Optional 'Link' header proposed to the api consumer
                    for navigation purpose. Possible value is 'next'.""")
 @doc.param('per_page', default=10,
-           doc="""Default parameter used to group result by page of the specified
-number.""")
+           doc="""Default parameter used to group result by page of the
+specified number.""")
 @doc.raises(exc=doc.excs.badinput,
             doc='Raised if sha1_git or prev_sha1s is not well formed')
 @doc.raises(exc=doc.excs.notfound,
