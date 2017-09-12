f370df3e2c5c2dd96393af81bfc676ef8b7af2d5
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-09-12 10:04:28 +0200

    swh.web.config: Set config default as near as production

--- swh/web/config.py
+++ swh/web/config.py
@@ -15,8 +15,8 @@ DEFAULT_CONFIG = {
     }),
     'log_dir': ('string', '/tmp/swh/log'),
     'debug': ('bool', False),
-    'host': ('string', '127.0.0.1'),
-    'port': ('int', 8000),
+    'host': ('string', '127.0.0.1'),  # development property
+    'port': ('int', 5003),            # development property
     'secret_key': ('string', 'development key'),
     'limiters': ('dict', {
         'cache_uri': None,  # production: memcached as cache (127.0.0.1:11211)
