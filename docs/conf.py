4a60e53325ef0a9141d0ba7f621eea21bfa0c522
Nicolas Dandrimont <nicolas@dandrimont.eu>
2017-09-15 14:56:30 +0200

    docs/conf: point to an existing settings module

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swh.web.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swh.web.settings.development")
 django.setup()
 
 from swh.docs.sphinx.conf import *  # NoQA
