a76713a6eef18d4fc6339f8ecb35fea353a806e0
Stefano Zacchiroli <zack@upsilon.cc>
2017-10-13 13:27:51 +0200

    docs: enable httpdomain sphinx extension
    note: this was enabled only for swh-web, but it really needs to also be in
    swh-docs, because we pull there docs from individual modules

--- docs/conf.py
+++ docs/conf.py
@@ -5,5 +5,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swh.web.settings.development")
 django.setup()
 
 from swh.docs.sphinx.conf import *  # NoQA
-
-extensions += ['sphinxcontrib.httpdomain'] # noqa
