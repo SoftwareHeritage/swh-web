50dc8ecf5675e63ebeb0bc18ead82aa26cc7d702
Antoine Lambert <antoine.lambert@inria.fr>
2018-02-23 10:58:22 +0100

    highlightjs-line-numbers: Upgrade to version 2.2.0
    This new release fixes multi-line strings and comments highlighting bugs
    (see https://github.com/wcoder/highlightjs-line-numbers.js/pull/22 and
    https://github.com/wcoder/highlightjs-line-numbers.js/pull/30).

--- swh/web/static/js/highlightjs-line-numbers/src/highlightjs-line-numbers.js
+++ swh/web/static/js/highlightjs-line-numbers/src/highlightjs-line-numbers.js
@@ -1,154 +1,177 @@
+// jshint multistr:true
+
 (function (w, d) {
-	'use strict';
+    'use strict';
 
-	var TABLE_NAME = 'hljs-ln',
-		LINE_NAME = 'hljs-ln-line',
-		CODE_BLOCK_NAME = 'hljs-ln-code',
-		NUMBERS_BLOCK_NAME = 'hljs-ln-numbers',
-		NUMBER_LINE_NAME = 'hljs-ln-n',
-		DATA_ATTR_NAME = 'data-line-number';
+    var TABLE_NAME = 'hljs-ln',
+        LINE_NAME = 'hljs-ln-line',
+        CODE_BLOCK_NAME = 'hljs-ln-code',
+        NUMBERS_BLOCK_NAME = 'hljs-ln-numbers',
+        NUMBER_LINE_NAME = 'hljs-ln-n',
+        DATA_ATTR_NAME = 'data-line-number',
+        BREAK_LINE_REGEXP = /\r\n|\r|\n/g;
 
-	// string format
-	// https://wcoder.github.io/notes/string-format-for-string-formating-in-javascript
-	var format = function (str, args) {
-		return str.replace(/\{(\d+)\}/g, function(m, n){
-			return args[n] ? args[n] : m;
-		});
-	};
+    if (w.hljs) {
+        w.hljs.initLineNumbersOnLoad = initLineNumbersOnLoad;
+        w.hljs.lineNumbersBlock = lineNumbersBlock;
 
-	if (w.hljs) {
-		w.hljs.initLineNumbersOnLoad = initLineNumbersOnLoad;
-		w.hljs.lineNumbersBlock = lineNumbersBlock;
-
-		addStyles();
-	} else {
-		w.console.error('highlight.js not detected!');
-	}
-
-	function addStyles () {
-		var css = d.createElement('style');
-		css.type = 'text/css';
-		css.innerHTML = format(
-			'.{0}{border-collapse:collapse}\
-			.{0} td{padding:0}\
-			.{1}:before{content:attr({2})}',
-		[
-			TABLE_NAME,
-			NUMBER_LINE_NAME,
-			DATA_ATTR_NAME
-		]);
-		d.getElementsByTagName('head')[0].appendChild(css);
-	}
-
-	function initLineNumbersOnLoad (options) {
-		if (d.readyState === 'complete') {
-			documentReady(options);
-		} else {
-			w.addEventListener('DOMContentLoaded', function () {
-				documentReady(options);
-			});
-		}
-	}
-
-	function documentReady (options) {
-		try {
-			var blocks = d.querySelectorAll('code.hljs');
-
-			for (var i in blocks) {
-				if (blocks.hasOwnProperty(i)) {
-					lineNumbersBlock(blocks[i], options);
-				}
-			}
-		} catch (e) {
-			w.console.error('LineNumbers error: ', e);
-		}
-	}
-
-	function lineNumbersBlock (element, options) {
-		if (typeof element !== 'object') return;
-
-		// define options or set default
-		options = options || {
-			singleLine: false
-		};
-
-		// convert options
-		var firstLineIndex = !!options.singleLine ? 0 : 1;
-
-        var text = changeMultilineComments(element);
-        var lines = getLines(text);
-
-		if (lines.length > firstLineIndex) {
-			var html = '';
-
-			for (var i = 0, l = lines.length; i < l; i++) {
-				html += format(
-					'<tr>\
-						<td class="{0}">\
-							<div class="{1} {2}" {3}="{5}"></div>\
-						</td>\
-						<td class="{4}">\
-							<div class="{1}">{6}</div>\
-						</td>\
-					</tr>',
-				[
-					NUMBERS_BLOCK_NAME,
-					LINE_NAME,
-					NUMBER_LINE_NAME,
-					DATA_ATTR_NAME,
-					CODE_BLOCK_NAME,
-					i + 1,
-					lines[i].length > 0 ? lines[i] : ' '
-				]);
-			}
-
-			element.innerHTML = format('<table class="{0}">{1}</table>', [ TABLE_NAME, html ]);
-		}
-	}
-
-	function duplicateMultilineTag(element){
-		var result = '';
-		var lines = element.innerText.split(/\r\n|\r|\n/g);
-		for(var i = 0; i<lines.length; i++){
-			element.innerText = lines[i];
-			result += element.outerHTML;
-			if(i < lines.length -1)
-				result += '\n';
-		}
-		return result;
-	}
-
-    function escapeHtml(unsafe) {
-        return unsafe.replace(/&/g, "&amp;")
-                     .replace(/</g, "&lt;")
-                     .replace(/>/g, "&gt;")
-                     .replace(/"/g, "&quot;")
-                     .replace(/'/g, "&#039;");
+        addStyles();
+    } else {
+        w.console.error('highlight.js not detected!');
     }
 
-	function changeMultilineComments(element) {
-		var result = '';
-		var children = element.childNodes;
-		for(var child in children) {
-			if(children.hasOwnProperty(child)) {
-                if(children[child].className === "hljs-comment" ||
-                   children[child].className === "hljs-string") {
-					result += duplicateMultilineTag(children[child]);
-				}
-				else if(children[child].childNodes.length) {
-					result += children[child].outerHTML;
-				}
-				else {
-					result += escapeHtml(children[child].textContent);
-				}
-			}
-        }
-		return result;
-	}
+    function addStyles () {
+        var css = d.createElement('style');
+        css.type = 'text/css';
+        css.innerHTML = format(
+            '.{0}{border-collapse:collapse}\
+            .{0} td{padding:0}\
+            .{1}:before{content:attr({2})}',
+        [
+            TABLE_NAME,
+            NUMBER_LINE_NAME,
+            DATA_ATTR_NAME
+        ]);
+        d.getElementsByTagName('head')[0].appendChild(css);
+    }
 
-	function getLines(text) {
-		if (text.length === 0) return [];
-		return text.split(/\r\n|\r|\n/g);
-	}
+    function initLineNumbersOnLoad (options) {
+        if (d.readyState === 'complete') {
+            documentReady(options);
+        } else {
+            w.addEventListener('DOMContentLoaded', function () {
+                documentReady(options);
+            });
+        }
+    }
+
+    function documentReady (options) {
+        try {
+            var blocks = d.querySelectorAll('code.hljs');
+
+            for (var i in blocks) {
+                if (blocks.hasOwnProperty(i)) {
+                    lineNumbersBlock(blocks[i], options);
+                }
+            }
+        } catch (e) {
+            w.console.error('LineNumbers error: ', e);
+        }
+    }
+
+    function lineNumbersBlock (element, options) {
+        if (typeof element !== 'object') return;
+
+        // define options or set default
+        options = options || {
+            singleLine: false
+        };
+
+        // convert options
+        var firstLineIndex = !!options.singleLine ? 0 : 1;
+
+        async(function () {
+
+            duplicateMultilineNodes(element);
+
+            element.innerHTML = addLineNumbersBlockFor(element.innerHTML, firstLineIndex);
+        });
+    }
+
+    function addLineNumbersBlockFor (inputHtml, firstLineIndex) {
+
+        var lines = getLines(inputHtml);
+
+        if (lines.length > firstLineIndex) {
+            var html = '';
+
+            for (var i = 0, l = lines.length; i < l; i++) {
+                html += format(
+                    '<tr>\
+                        <td class="{0}">\
+                            <div class="{1} {2}" {3}="{5}"></div>\
+                        </td>\
+                        <td class="{4}">\
+                            <div class="{1}">{6}</div>\
+                        </td>\
+                    </tr>',
+                [
+                    NUMBERS_BLOCK_NAME,
+                    LINE_NAME,
+                    NUMBER_LINE_NAME,
+                    DATA_ATTR_NAME,
+                    CODE_BLOCK_NAME,
+                    i + 1,
+                    lines[i].length > 0 ? lines[i] : ' '
+                ]);
+            }
+
+            return format('<table class="{0}">{1}</table>', [ TABLE_NAME, html ]);
+        }
+
+        return inputHtml;
+    }
+
+    /**
+     * Recursive method for fix multi-line elements implementation in highlight.js
+     * Doing deep passage on child nodes.
+     * @param {HTMLElement} element
+     */
+    function duplicateMultilineNodes (element) {
+        var nodes = element.childNodes;
+        for (var node in nodes){
+            if (nodes.hasOwnProperty(node)) {
+                var child = nodes[node];
+                if (getLinesCount(child.textContent) > 0) {
+                    if (child.childNodes.length > 0) {
+                        duplicateMultilineNodes(child);
+                    } else {
+                        duplicateMultilineNode(child);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Method for fix multi-line elements implementation in highlight.js
+     * @param {HTMLElement} element
+     */
+    function duplicateMultilineNode (element) {
+        var className = element.parentNode.className;
+
+        if ( ! /hljs-/.test(className)) return;
+
+        var lines = getLines(element.textContent);
+
+        for (var i = 0, result = ''; i < lines.length; i++) {
+            result += format('<span class="{0}">{1}</span>\n', [ className, lines[i] ]);
+        }
+        element.parentNode.innerHTML = result.trim();
+    }
+
+    function getLines (text) {
+        if (text.length === 0) return [];
+        return text.split(BREAK_LINE_REGEXP);
+    }
+
+    function getLinesCount (text) {
+        return (text.trim().match(BREAK_LINE_REGEXP) || []).length;
+    }
+
+    function async (func) {
+        w.setTimeout(func, 0);
+    }
+
+    /**
+     * {@link https://wcoder.github.io/notes/string-format-for-string-formating-in-javascript}
+     * @param {string} format
+     * @param {array} args
+     */
+    function format (format, args) {
+        return format.replace(/\{(\d+)\}/g, function(m, n){
+            return args[n] ? args[n] : m;
+        });
+    }
 
 }(window, document));
