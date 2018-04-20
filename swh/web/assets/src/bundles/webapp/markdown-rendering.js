d0679d8dce1ef9d8b080fb9d250287ff5d23b8e5
Antoine Lambert <antoine.lambert@inria.fr>
2018-04-20 15:43:49 +0200

    browse: Fix Basic Authorization Headers not sent by default with Fetch API

--- swh/web/assets/src/bundles/webapp/markdown-rendering.js
+++ swh/web/assets/src/bundles/webapp/markdown-rendering.js
@@ -4,7 +4,7 @@ export async function renderMarkdown(domElt, markdownDocUrl) {
 
   $(document).ready(() => {
     let converter = new showdown.Converter({tables: true});
-    fetch(markdownDocUrl)
+    fetch(markdownDocUrl, {credentials: 'same-origin'})
       .then(response => response.text())
       .then(data => {
         $(domElt).html(converter.makeHtml(data));
