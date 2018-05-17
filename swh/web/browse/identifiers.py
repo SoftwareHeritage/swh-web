d666e8c1b1379b0e18f5d224fdc196f6746b1ee0
Antoine Lambert <antoine.lambert@inria.fr>
2018-05-17 17:03:31 +0200

    persistent identifiers: Resolve optional parts
    This commits adds support for optional parts of persistent identifiers
    in swh-web in order to add origin context when resolving the swh object
    to browse. It is also possible to set lines to highlight for swh content
    object.
    
    Also to be noted, persistent id urls are now rooted to / instead of
    /browse. Nevertheless backward compatibility is maintained for HAL.
    
    Closes T1042

--- swh/web/browse/identifiers.py
+++ swh/web/browse/identifiers.py
@@ -7,46 +7,52 @@ from django.shortcuts import redirect
 
 from swh.model.identifiers import parse_persistent_identifier
 
-from swh.web.browse.browseurls import browse_route
 from swh.web.common.utils import reverse
 from swh.web.common.exc import BadInputExc, handle_view_exception
 
 
-@browse_route(r'(?P<swh_id>swh:[0-9]+:[a-z]+:[0-9a-f]+)/',
-              view_name='browse-swh-id')
 def swh_id_browse(request, swh_id):
     """
     Django view enabling to browse the SWH archive using
     :ref:`persistent-identifiers`.
 
-    The url that points to it is :http:get:`/browse/(swh_id)/`.
+    The url that points to it is :http:get:`/(swh_id)/`.
     """
     try:
         swh_id_parsed = parse_persistent_identifier(swh_id)
         object_type = swh_id_parsed['object_type']
         object_id = swh_id_parsed['object_id']
         view_url = None
+        query_params = request.GET.copy()
+        if 'origin' in swh_id_parsed['metadata']:
+            query_params['origin'] = swh_id_parsed['metadata']['origin']
         if object_type == 'cnt':
             query_string = 'sha1_git:' + object_id
+            fragment = ''
+            if 'lines' in swh_id_parsed['metadata']:
+                lines = swh_id_parsed['metadata']['lines'].split('-')
+                fragment += '#L' + lines[0]
+                if len(lines) > 1:
+                    fragment += '-L' + lines[1]
             view_url = reverse('browse-content',
                                kwargs={'query_string': query_string},
-                               query_params=request.GET)
+                               query_params=query_params) + fragment
         elif object_type == 'dir':
             view_url = reverse('browse-directory',
                                kwargs={'sha1_git': object_id},
-                               query_params=request.GET)
+                               query_params=query_params)
         elif object_type == 'rel':
             view_url = reverse('browse-release',
                                kwargs={'sha1_git': object_id},
-                               query_params=request.GET)
+                               query_params=query_params)
         elif object_type == 'rev':
             view_url = reverse('browse-revision',
                                kwargs={'sha1_git': object_id},
-                               query_params=request.GET)
+                               query_params=query_params)
         elif object_type == 'snp':
             view_url = reverse('browse-snapshot',
                                kwargs={'snapshot_id': object_id},
-                               query_params=request.GET)
+                               query_params=query_params)
         else:
             msg = '\'%s\' is not a valid SWH persistent identifier!' % swh_id
             raise BadInputExc(msg)
