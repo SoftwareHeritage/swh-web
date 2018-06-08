ab4bd29e7704d406142f6d0f061a67ad377c06f5
Antoine Lambert <antoine.lambert@inria.fr>
2018-06-08 13:58:47 +0200

    api: Add rate limiting headers in HTTP responses
    Closes T1085

--- swh/web/tests/common/test_throttling.py
+++ swh/web/tests/common/test_throttling.py
@@ -10,14 +10,16 @@ from swh.web.settings.tests import (
 )
 
 from django.test import TestCase
+from django.conf.urls import url
 from django.core.cache import cache
+from django.test.utils import override_settings
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.test import APIRequestFactory
 from rest_framework.decorators import api_view
 
-from nose.tools import istest
+from nose.tools import istest, nottest
 
 from swh.web.common.throttling import (
     SwhWebRateThrottle, throttle_scope
@@ -64,6 +66,15 @@ def mock_view_scope3(request):
         return Response('bar_post')
 
 
+urlpatterns = [
+    url(r'^scope1_class$', MockViewScope1.as_view()),
+    url(r'^scope2_func$', mock_view_scope2),
+    url(r'^scope3_class$', MockViewScope3.as_view()),
+    url(r'^scope3_func$', mock_view_scope3)
+]
+
+
+@override_settings(ROOT_URLCONF=__name__)
 class ThrottlingTests(TestCase):
     def setUp(self):
         """
@@ -72,43 +83,60 @@ class ThrottlingTests(TestCase):
         cache.clear()
         self.factory = APIRequestFactory()
 
+    @nottest
+    def check_response(self, response, status_code,
+                       limit=None, remaining=None):
+        assert response.status_code == status_code
+        if limit is not None:
+            assert response['X-RateLimit-Limit'] == str(limit)
+        else:
+            assert 'X-RateLimit-Limit' not in response
+        if remaining is not None:
+            assert response['X-RateLimit-Remaining'] == str(remaining)
+        else:
+            assert 'X-RateLimit-Remaining' not in response
+
     @istest
     def scope1_requests_are_throttled(self):
         """
         Ensure request rate is limited in scope1
         """
-        request = self.factory.get('/')
-        for _ in range(scope1_limiter_rate):
-            response = MockViewScope1.as_view()(request)
-            assert response.status_code == 200
-        response = MockViewScope1.as_view()(request)
-        assert response.status_code == 429
+        for i in range(scope1_limiter_rate):
+            response = self.client.get('/scope1_class')
+            self.check_response(response, 200, scope1_limiter_rate,
+                                scope1_limiter_rate - i - 1)
 
-        request = self.factory.post('/')
-        for _ in range(scope1_limiter_rate_post):
-            response = MockViewScope1.as_view()(request)
-            assert response.status_code == 200
-        response = MockViewScope1.as_view()(request)
-        assert response.status_code == 429
+        response = self.client.get('/scope1_class')
+        self.check_response(response, 429, scope1_limiter_rate, 0)
+
+        for i in range(scope1_limiter_rate_post):
+            response = self.client.post('/scope1_class')
+            self.check_response(response, 200, scope1_limiter_rate_post,
+                                scope1_limiter_rate_post - i - 1)
+
+        response = self.client.post('/scope1_class')
+        self.check_response(response, 429, scope1_limiter_rate_post, 0)
 
     @istest
     def scope2_requests_are_throttled(self):
         """
         Ensure request rate is limited in scope2
         """
-        request = self.factory.get('/')
-        for _ in range(scope2_limiter_rate):
-            response = mock_view_scope2(request)
-            assert response.status_code == 200
-        response = mock_view_scope2(request)
-        assert response.status_code == 429
+        for i in range(scope2_limiter_rate):
+            response = self.client.get('/scope2_func')
+            self.check_response(response, 200, scope2_limiter_rate,
+                                scope2_limiter_rate - i - 1)
 
-        request = self.factory.post('/')
-        for _ in range(scope2_limiter_rate_post):
-            response = mock_view_scope2(request)
-            assert response.status_code == 200
-        response = mock_view_scope2(request)
-        assert response.status_code == 429
+        response = self.client.get('/scope2_func')
+        self.check_response(response, 429, scope2_limiter_rate, 0)
+
+        for i in range(scope2_limiter_rate_post):
+            response = self.client.post('/scope2_func')
+            self.check_response(response, 200, scope2_limiter_rate_post,
+                                scope2_limiter_rate_post - i - 1)
+
+        response = self.client.post('/scope2_func')
+        self.check_response(response, 429, scope2_limiter_rate_post, 0)
 
     @istest
     def scope3_requests_are_throttled_exempted(self):
@@ -116,22 +144,18 @@ class ThrottlingTests(TestCase):
         Ensure request rate is not limited in scope3 as
         requests coming from localhost are exempted from rate limit.
         """
-        request = self.factory.get('/')
         for _ in range(scope3_limiter_rate+1):
-            response = MockViewScope3.as_view()(request)
-            assert response.status_code == 200
+            response = self.client.get('/scope3_class')
+            self.check_response(response, 200)
 
-        request = self.factory.post('/')
         for _ in range(scope3_limiter_rate_post+1):
-            response = MockViewScope3.as_view()(request)
-            assert response.status_code == 200
+            response = self.client.post('/scope3_class')
+            self.check_response(response, 200)
 
-        request = self.factory.get('/')
         for _ in range(scope3_limiter_rate+1):
-            response = mock_view_scope3(request)
-            assert response.status_code == 200
+            response = self.client.get('/scope3_func')
+            self.check_response(response, 200)
 
-        request = self.factory.post('/')
         for _ in range(scope3_limiter_rate_post+1):
-            response = mock_view_scope3(request)
-            assert response.status_code == 200
+            response = self.client.post('/scope3_func')
+            self.check_response(response, 200)
