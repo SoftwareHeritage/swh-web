4e36feab0b669ff10d61cf030f47975b3e13aa2e
David Douard <david.douard@sdfa3.org>
2018-10-05 14:28:18 +0200

    setup: prepare for pypi upload
    related to T1242

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+# Copyright (C) 2015-2018  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU Affero General Public License v3 or later
+# See top-level LICENSE file for more information
 
-import os
 from setuptools import setup, find_packages
+
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 def parse_requirements(name=None):
@@ -11,7 +23,7 @@ def parse_requirements(name=None):
         reqf = 'requirements.txt'
 
     requirements = []
-    if not os.path.exists(reqf):
+    if not path.exists(reqf):
         return requirements
 
     with open(reqf) as f:
@@ -26,6 +38,8 @@ def parse_requirements(name=None):
 setup(
     name='swh.web',
     description='Software Heritage Web UI',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Software Heritage developers',
     author_email='swh-devel@inria.fr',
     url='https://forge.softwareheritage.org/diffusion/DWUI/',
@@ -36,4 +50,17 @@ setup(
     extras_require={'testing': parse_requirements('test')},
     vcversioner={},
     include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",  # noqa
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+    ],
+    project_urls={
+        'Bug Reports': 'https://forge.softwareheritage.org/maniphest',
+        'Funding': 'https://www.softwareheritage.org/donate',
+        'Source': 'https://forge.softwareheritage.org/source/swh-web',
+    },
 )
