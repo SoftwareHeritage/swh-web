a261bcd42c08e3d63d3146fb6ad01aa8056d77b1
Antoine Lambert <antoine.lambert@inria.fr>
2019-02-20 17:45:40 +0100

    tests.browse.identifiers: Use hypothesis instead of stubbed data
    Related T1271

--- swh/web/tests/browse/views/data/directory_test_data.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2017-2018  The Software Heritage developers
-# See the AUTHORS file at the top-level directory of this distribution
-# License: GNU Affero General Public License version 3, or any later version
-# See top-level LICENSE file for more information
-
-# flake8: noqa
-
-stub_root_directory_sha1 = '08e8329257dad3a3ef7adea48aa6e576cd82de5b'
