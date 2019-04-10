275325294bb5d8e55a1f47964d54472b39d96276
Antoine Lambert <antoine.lambert@inria.fr>
2019-04-10 14:18:23 +0200

    config: Allow to specify development database file
    Closes T1639

--- swh/web/settings/common.py
+++ swh/web/settings/common.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'swh.web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'db.sqlite3'),
+        'NAME': swh_web_config['development_db'],
     }
 }
 
