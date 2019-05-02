bd58777442e2495aa2d459868c63d649ad20b7a2
Antoine Lambert <antoine.lambert@inria.fr>
2019-05-02 14:04:39 +0200

    common.origin_save: Update scheduler loading task names
    Related T1508

--- swh/web/common/origin_save.py
+++ swh/web/common/origin_save.py
@@ -85,9 +85,9 @@ def can_save_origin(origin_url):
 # map origin type to scheduler task
 # TODO: do not hardcode the task name here (T1157)
 _origin_type_task = {
-    'git': 'origin-update-git',
-    'hg': 'origin-load-hg',
-    'svn': 'origin-load-svn'
+    'git': 'load-git',
+    'hg': 'load-hg',
+    'svn': 'load-svn'
 }
 
 
