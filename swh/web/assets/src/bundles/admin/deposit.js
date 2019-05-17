7a671af936f553b1b5465b00aa83849e2229ac1b
Antoine Lambert <antoine.lambert@inria.fr>
2019-05-17 14:25:41 +0200

    admin/deposit: Improve reporting graphical interface
      - extract deposit origin from revision pid
    
      - add new columns to the table related to swh revision / directory
    
      - rename some table columns
    
      - enable to dynamically show / hide table columns
    
    Closes T1490
    Closes T1719

--- swh/web/assets/src/bundles/admin/deposit.js
+++ swh/web/assets/src/bundles/admin/deposit.js
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2018-2019  The Software Heritage developers
+ * See the AUTHORS file at the top-level directory of this distribution
+ * License: GNU Affero General Public License version 3, or any later version
+ * See top-level LICENSE file for more information
+ */
+
+function genSwhLink(data, type) {
+  if (type === 'display') {
+    if (data && data.startsWith('swh')) {
+      let browseUrl = Urls.browse_swh_id(data);
+      return `<a href="${browseUrl}">${data}</a>`;
+    }
+  }
+  return data;
+}
+
 export function initDepositAdmin() {
   let depositsTable;
   $(document).ready(() => {
@@ -15,12 +32,15 @@ export function initDepositAdmin() {
             name: 'id'
           },
           {
-            data: 'external_id',
-            name: 'external_id',
+            data: 'swh_id_context',
+            name: 'swh_id_context',
             render: (data, type, row) => {
-              if (type === 'display') {
-                if (data && data.startsWith('hal')) {
-                  return `<a href="https://hal.archives-ouvertes.fr/${data}">${data}</a>`;
+              if (data && type === 'display') {
+                let originPattern = ';origin=';
+                let originPatternIdx = data.indexOf(originPattern);
+                if (originPatternIdx !== -1) {
+                  let originUrl = data.slice(originPatternIdx + originPattern.length);
+                  return `<a href="${originUrl}">${originUrl}</a>`;
                 }
               }
               return data;
@@ -54,26 +74,62 @@ export function initDepositAdmin() {
               }
               return data;
             },
+            orderable: false,
+            visible: false
+          },
+          {
+            data: 'swh_anchor_id',
+            name: 'swh_anchor_id',
+            render: (data, type, row) => {
+              return genSwhLink(data, type);
+            },
             orderable: false
+          },
+          {
+            data: 'swh_anchor_id_context',
+            name: 'swh_anchor_id_context',
+            render: (data, type, row) => {
+              return genSwhLink(data, type);
+            },
+            orderable: false,
+            visible: false
           },
           {
             data: 'swh_id',
             name: 'swh_id',
             render: (data, type, row) => {
-              if (type === 'display') {
-                if (data && data.startsWith('swh')) {
-                  let browseUrl = Urls.browse_swh_id(data);
-                  return `<a href="${browseUrl}">${data}</a>`;
-                }
-              }
-              return data;
-            }
+              return genSwhLink(data, type);
+            },
+            orderable: false,
+            visible: false
+          },
+          {
+            data: 'swh_id_context',
+            name: 'swh_id_context',
+            render: (data, type, row) => {
+              return genSwhLink(data, type);
+            },
+            orderable: false,
+            visible: false
           }
         ],
+        scrollX: true,
         scrollY: '50vh',
         scrollCollapse: true,
         order: [[0, 'desc']]
       });
     depositsTable.draw();
   });
+
+  $('a.toggle-col').on('click', function(e) {
+    e.preventDefault();
+    var column = depositsTable.column($(this).attr('data-column'));
+    column.visible(!column.visible());
+    if (column.visible()) {
+      $(this).removeClass('col-hidden');
+    } else {
+      $(this).addClass('col-hidden');
+    }
+  });
+
 }
