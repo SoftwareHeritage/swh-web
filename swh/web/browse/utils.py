SoftwareHeritage/swh-web

commit a2759fec6009471ae8f966596dde10e4b6591767

browse: Robustify mimetype/encoding detection for contents

swh/web/browse/utils.py
@@ -12,6 +12,8 @@ import stat
 from django.core.cache import cache
 from django.utils.safestring import mark_safe
 
+from importlib import reload
+
 from swh.web.common import highlightjs, service
 from swh.web.common.exc import NotFoundExc
 from swh.web.common.utils import (
@@ -66,8 +68,22 @@ def get_mimetype_and_encoding_for_content(content):
         associated to the provided content.
 
     """
-    magic_result = magic.detect_from_content(content)
-    return magic_result.mime_type, magic_result.encoding
+    while True:
+        try:
+            magic_result = magic.detect_from_content(content)
+            mime_type = magic_result.mime_type
+            encoding = magic_result.encoding
+            break
+        except Exception as exc:
+            # workaround an issue with the magic module who can fail
+            # if detect_from_content is called multiple times in
+            # a short amount of time
+            if 'too many values to unpack' in str(exc):
+                reload(magic)
+            else:
+                break
+
+    return mime_type, encoding
 
 
 def request_content(query_string):
@@ -106,10 +122,17 @@ def request_content(query_string):
     content_data['encoding'] = encoding
 
     # encode textual content to utf-8 if needed
-    if mimetype.startswith('text/') and 'ascii' not in encoding \
-       and encoding not in ['utf-8', 'binary']:
-        content_data['raw_data'] = \
-            content_data['raw_data'].decode(encoding).encode('utf-8')
+    if mimetype.startswith('text/'):
+        # probably a malformed UTF-8 content, reencode it
+        # by replacing invalid chars with a substitution one
+        if encoding == 'unknown-8bit':
+            content_data['raw_data'] = \
+                content_data['raw_data'].decode('utf-8', 'replace')\
+                                        .encode('utf-8')
+        elif 'ascii' not in encoding and encoding not in ['utf-8', 'binary']:
+            content_data['raw_data'] = \
+                content_data['raw_data'].decode(encoding, 'replace')\
+                                        .encode('utf-8')
 
     if language:
         content_data['language'] = language['lang']

commit cd747b23ec7f62439c88476ea3e84fb8f46fa536

browse/utils: Properly fix the issue with file-magic
Finally manage to remove the ugly hack I introduced in a2759fec6009. Turns out
that calls to file-magic API are not thread-safe so they must be protected
with a threading.Lock instance.

swh/web/browse/utils.py
@@ -4,17 +4,17 @@
 # See top-level LICENSE file for more information
 
 import base64
-from collections import defaultdict
 import magic
 import pypandoc
 import stat
 import textwrap
 
+from collections import defaultdict
+from threading import Lock
+
 from django.core.cache import cache
 from django.utils.safestring import mark_safe
 from django.utils.html import escape
-
-from importlib import reload
 
 from swh.model.identifiers import persistent_identifier
 from swh.web.common import highlightjs, service
@@ -69,6 +69,9 @@ def get_directory_entries(sha1_git):
     return dirs, files
 
 
+_lock = Lock()
+
+
 def get_mimetype_and_encoding_for_content(content):
     """Function that returns the mime type and the encoding associated to
     a content buffer using the magic module under the hood.
@@ -93,17 +96,15 @@ def get_mimetype_and_encoding_for_content(content):
     else:
         # TODO: Remove that code when production environment is upgraded
         #       to debian buster
-        while True:
-            try:
-                magic_result = magic.detect_from_content(content)
-                mime_type = magic_result.mime_type
-                encoding = magic_result.encoding
-                break
-            except Exception:
-                # workaround an issue with the magic module who can fail
-                # if detect_from_content is called multiple times in
-                # a short amount of time
-                reload(magic)
+
+        # calls to the file-magic API are not thread-safe so they must
+        # be protected with a Lock to guarantee they will succeed
+        _lock.acquire()
+        magic_result = magic.detect_from_content(content)
+        _lock.release()
+        mime_type = magic_result.mime_type
+        encoding = magic_result.encoding
+
     return mime_type, encoding
 
 