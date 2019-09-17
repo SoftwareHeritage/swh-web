SoftwareHeritage/swh-web

commit 214c1781f7a7bc4285072ab948a563ee87e8d6f1

settings.common: Refine Django logs formatting and filtering

swh/web/settings/common.py
@@ -178,8 +178,12 @@ LOGGING = {
         },
     },
     'formatters': {
+        'simple': {
+            'format': '[%(asctime)s] [%(levelname)s] %(message)s',
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
         'verbose': {
-            'format': '[%(asctime)s] [%(levelname)s] %(request)s %(status_code)s', # noqa
+            'format': '[%(asctime)s] [%(levelname)s] %(name)s.%(funcName)s:%(lineno)s - %(message)s', # noqa
             'datefmt': "%d/%b/%Y %H:%M:%S"
         },
     },
@@ -188,9 +192,23 @@ LOGGING = {
             'level': 'DEBUG',
             'filters': ['require_debug_true'],
             'class': 'logging.StreamHandler',
+            'formatter': 'simple'
         },
         'file': {
-            'level': 'INFO',
+            'level': 'WARNING',
+            'filters': ['require_debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(swh_web_config['log_dir'], 'swh-web.log'),
+            'formatter': 'simple'
+        },
+        'console_verbose': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'file_verbose': {
+            'level': 'WARNING',
             'filters': ['require_debug_false'],
             'class': 'logging.FileHandler',
             'filename': os.path.join(swh_web_config['log_dir'], 'swh-web.log'),
@@ -201,20 +219,23 @@ LOGGING = {
         },
     },
     'loggers': {
+        '': {
+            'handlers': ['console_verbose', 'file_verbose'],
+            'level': 'DEBUG' if DEBUG else 'WARNING',
+        },
         'django': {
             'handlers': ['console', 'file'],
-            'level': 'DEBUG' if DEBUG else 'INFO',
-            'propagate': True,
+            'level': 'DEBUG' if DEBUG else 'WARNING',
+            'propagate': False,
         },
         'django.request': {
-            'handlers': ['file'],
-            'level': 'DEBUG' if DEBUG else 'INFO',
+            'handlers': ['null'],
             'propagate': False,
         },
         'django.db.backends': {
             'handlers': ['null'],
             'propagate': False
-        }
+        },
     },
 }
 

commit 9b0cd396c7a0f0edab0ba7117a63271c4fce81d0

settings: Restore django request logging in production
The feature has been broken in 214c1781f7a7. I should have tested the changes when
the application is served using gunicorn workers.

swh/web/settings/common.py
@@ -178,6 +178,10 @@ LOGGING = {
         },
     },
     'formatters': {
+        'request': {
+            'format': '[%(asctime)s] [%(levelname)s] %(request)s %(status_code)s', # noqa
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
         'simple': {
             'format': '[%(asctime)s] [%(levelname)s] %(message)s',
             'datefmt': "%d/%b/%Y %H:%M:%S"
@@ -200,6 +204,13 @@ LOGGING = {
             'class': 'logging.FileHandler',
             'filename': os.path.join(swh_web_config['log_dir'], 'swh-web.log'),
             'formatter': 'simple'
+        },
+        'file_request': {
+            'level': 'WARNING',
+            'filters': ['require_debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(swh_web_config['log_dir'], 'swh-web.log'),
+            'formatter': 'request'
         },
         'console_verbose': {
             'level': 'DEBUG',
@@ -224,12 +235,13 @@ LOGGING = {
             'level': 'DEBUG' if DEBUG else 'WARNING',
         },
         'django': {
-            'handlers': ['console', 'file'],
+            'handlers': ['console'],
             'level': 'DEBUG' if DEBUG else 'WARNING',
             'propagate': False,
         },
         'django.request': {
-            'handlers': ['null'],
+            'handlers': ['file_request'],
+            'level': 'DEBUG' if DEBUG else 'WARNING',
             'propagate': False,
         },
         'django.db.backends': {