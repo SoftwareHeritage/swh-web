25d37d2195ef3de0ac00d7f327be9140f23e3c6b
Antoine Lambert <antoine.lambert@inria.fr>
2019-12-12 15:43:15 +0100

    tests/data: Clean git loader configuration

--- swh/web/tests/data.py
+++ swh/web/tests/data.py
@@ -35,20 +35,8 @@ _TEST_LOADER_CONFIG = {
     'storage': {
         'cls': 'memory'
     },
-    'send_contents': True,
-    'send_directories': True,
-    'send_revisions': True,
-    'send_releases': True,
-    'send_snapshot': True,
-
-    'content_size_limit': 100 * 1024 * 1024,
-    'content_packet_size': 10,
-    'content_packet_size_bytes': 100 * 1024 * 1024,
-    'directory_packet_size': 10,
-    'revision_packet_size': 10,
-    'release_packet_size': 10,
-
     'save_data': False,
+    'max_content_size': 100 * 1024 * 1024,
 }
 
 # Base content indexer configuration
