f4a7ed735d1add5a3189fe11516f806369f49718
Antoine Lambert <antoine.lambert@inria.fr>
2020-01-17 11:40:19 +0100

    common/origin_save: Fix elasticsearch total hits extraction
    Since the upgrade to elasticsearch 7.x in production, the parsing of the total hits
    value was failing due to a breaking change in the JSON response format.

--- swh/web/common/origin_save.py
+++ swh/web/common/origin_save.py
@@ -511,7 +511,7 @@ def get_save_origin_task_info(save_request_id):
                                        'sort': ['@timestamp']},
                                  timeout=30)
         results = json.loads(response.text)
-        if results['hits']['total'] >= 1:
+        if results['hits']['total']['value'] >= 1:
             task_run_info = results['hits']['hits'][-1]['_source']
             if 'swh_logging_args_runtime' in task_run_info:
                 duration = task_run_info['swh_logging_args_runtime']
