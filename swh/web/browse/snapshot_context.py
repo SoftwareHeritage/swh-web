SoftwareHeritage/swh-web

commit 09e18940394e6b676b245017c2507f8b9b0638b3

templates/directory-display: Display message for missing directory/revision
That message can only be displayed when browsing a directory in a snapshot
context, for instance when browsing a branch whith a non resolvable revision.

Related to T2353

swh/web/browse/snapshot_context.py
@@ -740,6 +740,13 @@ def browse_snapshot_directory(
     browse_rev_link = gen_revision_link(revision_id)
     browse_snp_link = gen_snapshot_link(snapshot_id)
 
+    revision_found = True
+    if sha1_git is None:
+        try:
+            service.lookup_revision(revision_id)
+        except NotFoundExc:
+            revision_found = False
+
     dir_metadata = {
         "directory": sha1_git,
         "context-independent directory": browse_dir_link,
@@ -748,6 +755,7 @@ def browse_snapshot_directory(
         "sum of regular file sizes": sum_file_sizes,
         "path": dir_path,
         "revision": revision_id,
+        "revision_found": revision_found,
         "context-independent revision": browse_rev_link,
         "snapshot": snapshot_id,
         "context-independent snapshot": browse_snp_link,

swh/web/tests/browse/views/test_origin.py
@@ -3,6 +3,7 @@
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+from datetime import datetime
 import random
 import re
 import string
@@ -12,7 +13,11 @@ from django.utils.html import escape
 from hypothesis import given
 
 from swh.model.hashutil import hash_to_bytes
-from swh.model.model import Snapshot
+from swh.model.model import (
+    Snapshot,
+    SnapshotBranch,
+    TargetType,
+)
 from swh.web.browse.snapshot_context import process_snapshot_branches
 from swh.web.common.exc import NotFoundExc
 from swh.web.common.identifiers import get_swh_persistent_id
@@ -34,6 +39,7 @@ from swh.web.tests.strategies import (
     revisions,
     origin_with_releases,
     release as existing_release,
+    unknown_revision,
 )
 
 
@@ -645,6 +651,39 @@ def test_origin_release_browse_not_found(client, origin):
     assert resp.status_code == 404
     assert re.search(
         f"Release {invalid_release_name}.*not found", resp.content.decode("utf-8")
+    )
+
+
+@given(new_origin(), unknown_revision())
+def test_origin_browse_directory_branch_with_non_resolvable_revision(
+    client, archive_data, new_origin, unknown_revision
+):
+    branch_name = "master"
+    snapshot = Snapshot(
+        branches={
+            branch_name.encode(): SnapshotBranch(
+                target=hash_to_bytes(unknown_revision), target_type=TargetType.REVISION,
+            )
+        }
+    )
+    new_origin = archive_data.origin_add([new_origin])[0]
+    archive_data.snapshot_add([snapshot])
+    visit = archive_data.origin_visit_add(new_origin["url"], datetime.now(), type="git")
+    archive_data.origin_visit_update(
+        new_origin["url"], visit.visit, status="full", snapshot=snapshot.id
+    )
+
+    url = reverse(
+        "browse-origin-directory",
+        url_args={"origin_url": new_origin["url"]},
+        query_params={"branch": branch_name},
+    )
+
+    resp = client.get(url)
+
+    assert resp.status_code == 200
+    assert_contains(
+        resp, f"Revision {unknown_revision } could not be found in the archive."
     )
 
 

swh/web/templates/includes/directory-display.html
@@ -53,6 +53,10 @@ See top-level LICENSE file for more information
     </table>
   </div>
   <hr class="mt-0 mb-2">
+{% elif "revision_found" in swh_object_metadata and swh_object_metadata.revision_found is False %}
+  <i>Revision {{ swh_object_metadata.revision }} could not be found in the archive.</i>
+  <br/>
+  <i>Its associated directory can not be displayed.</i>
 {% elif dirs|length == 0 and files|length == 0 %}
   <i>Directory is empty</i>
 {% endif %}

commit b6448adfe41116ac3cafc3fcc59a7a8502b34f0a

browse/snapshot_context: Do not lookup null revision in directory view
Fix a regression inroduced in 09e18940394e: when a revision id is null (for
instance when browsing an empty snapshot), do not attempt to lookup revision
data to avoid an error 500.

swh/web/browse/snapshot_context.py
@@ -736,7 +736,7 @@ def browse_snapshot_directory(
     browse_snp_link = gen_snapshot_link(snapshot_id)
 
     revision_found = True
-    if sha1_git is None:
+    if sha1_git is None and revision_id is not None:
         try:
             service.lookup_revision(revision_id)
         except NotFoundExc:

swh/web/tests/browse/views/test_origin.py
@@ -543,7 +543,7 @@ def test_browse_origin_content_unknown_visit(client, mocker, origin):
 
 
 @given(origin())
-def test_browse_origin_content_empty_snapshot(client, mocker, origin):
+def test_browse_origin_content_directory_empty_snapshot(client, mocker, origin):
     mock_snapshot_service = mocker.patch("swh.web.browse.snapshot_context.service")
     mock_get_origin_visit_snapshot = mocker.patch(
         "swh.web.browse.snapshot_context.get_origin_visit_snapshot"
@@ -554,18 +554,21 @@ def test_browse_origin_content_empty_snapshot(client, mocker, origin):
         "revision": 0,
         "release": 0,
     }
-    url = reverse(
-        "browse-origin-content",
-        url_args={"origin_url": origin["url"]},
-        query_params={"path": "baz"},
-    )
-    resp = client.get(url)
-    assert resp.status_code == 200
-    assert_template_used(resp, "browse/content.html")
-    assert re.search("snapshot.*is empty", resp.content.decode("utf-8"))
-    assert mock_get_origin_visit_snapshot.called
-    assert mock_snapshot_service.lookup_origin.called
-    assert mock_snapshot_service.lookup_snapshot_sizes.called
+
+    for browse_context in ("content", "directory"):
+
+        url = reverse(
+            f"browse-origin-{browse_context}",
+            url_args={"origin_url": origin["url"]},
+            query_params={"path": "baz"},
+        )
+        resp = client.get(url)
+        assert resp.status_code == 200
+        assert_template_used(resp, f"browse/{browse_context}.html")
+        assert re.search("snapshot.*is empty", resp.content.decode("utf-8"))
+        assert mock_get_origin_visit_snapshot.called
+        assert mock_snapshot_service.lookup_origin.called
+        assert mock_snapshot_service.lookup_snapshot_sizes.called
 
 
 @given(origin())