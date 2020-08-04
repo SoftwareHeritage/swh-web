SoftwareHeritage/swh-web

commit 8e0fa87b22d7196d72ab580882a61804e7c84912

templates/top-navigation: Update action buttons layout
Remove the Actions dropdown and replace with simple buttons instead: "Download",
"New snapshot" and "Show metadata".

Hide the "Download as git" and "Show metadata" action for non staff users.

Related to T2326

swh/web/templates/includes/show-metadata.html
@@ -1,5 +1,5 @@
 {% comment %}
-Copyright (C) 2017-2019  The Software Heritage developers
+Copyright (C) 2017-2020  The Software Heritage developers
 See the AUTHORS file at the top-level directory of this distribution
 License: GNU Affero General Public License version 3, or any later version
 See top-level LICENSE file for more information
@@ -7,9 +7,10 @@ See top-level LICENSE file for more information
 
 {% load swh_templatetags %}
 
-<div class="dropdown-submenu">
-  <a id="metadata-toggler" class="dropdown-item dropdown-left swh-popover-toggler" tabindex="-1" href="#" data-toggle="popover" data-popover-content="#metadata-popover"><i class="mdi mdi-table mdi-fw" aria-hidden="true"></i>Show metadata</a>
-</div>
+<button id="metadata-toggler" class="btn btn-default btn-sm swh-popover-toggler" data-toggle="popover" data-popover-content="#metadata-popover">
+  <i class="mdi mdi-table mdi-fw" aria-hidden="true"></i>
+  Show metadata
+</button>
 
 <div id="metadata-popover" style="display:none;">
   <div class="popover-heading">{{ swh_object_name }} metadata</div>

swh/web/templates/includes/top-navigation.html
@@ -106,18 +106,14 @@ See top-level LICENSE file for more information
         {% endfor %}
       </select>
     {% endif %}
-    {% if show_actions_menu %}
-      <button class="btn btn-default btn-sm dropdown-toggle" type="button" data-toggle="dropdown">
-        <i class="mdi mdi-menu mdi-fw" aria-hidden="true"></i>Actions
-        <span class="caret"></span>
-      </button>
-      <ul class="dropdown-menu dropdown-menu-right swh-browse-actions-menu">
-        {% if not snapshot_context or not snapshot_context.is_empty %}
-          {% include "includes/vault-create-tasks.html" %}
-        {% endif %}
+    {% if show_actions %}
+      {% if not snapshot_context or not snapshot_context.is_empty %}
+        {% include "includes/vault-create-tasks.html" %}
+      {% endif %}
+      {% include "includes/take-new-snapshot.html" %}
+      {% if user.is_staff %}
         {% include "includes/show-metadata.html" %}
-        {% include "includes/take-new-snapshot.html" %}
-      </ul>
+      {% endif %}
     {% endif %}
   </div>
 </div>

cypress/integration/vault.spec.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2019  The Software Heritage developers
+ * Copyright (C) 2019-2020  The Software Heritage developers
  * See the AUTHORS file at the top-level directory of this distribution
  * License: GNU Affero General Public License version 3, or any later version
  * See top-level LICENSE file for more information
@@ -13,11 +13,18 @@ const progressbarColors = {
   'done': 'rgb(92, 184, 92)'
 };
 
-function checkVaultCookingTask(objectType) {
-  cy.contains('button', 'Actions')
-    .click();
+function adminLogin() {
+  cy.visit('/admin/')
+    .get('input[name="username"]')
+    .type('admin')
+    .get('input[name="password"]')
+    .type('admin')
+    .get('form')
+    .submit();
+}
 
-  cy.contains('.dropdown-item', 'Download')
+function checkVaultCookingTask(objectType) {
+  cy.contains('button', 'Download')
     .click();
 
   cy.contains('.dropdown-item', objectType)
@@ -124,7 +131,8 @@ describe('Vault Cooking User Interface Tests', function() {
       response: this.genVaultDirCookingResponse('new')
     }).as('createVaultCookingTask');
 
-    checkVaultCookingTask('as tarball');
+    cy.contains('button', 'Download')
+      .click();
 
     cy.route({
       method: 'GET',
@@ -189,6 +197,7 @@ describe('Vault Cooking User Interface Tests', function() {
   });
 
   it('should create a revision cooking task and report its status', function() {
+    adminLogin();
     // Browse a revision
     cy.visit(this.revisionUrl);
 
@@ -358,7 +367,8 @@ describe('Vault Cooking User Interface Tests', function() {
     }).as('fetchCookedArchive');
 
     // Create a vault cooking task through the GUI
-    checkVaultCookingTask('as tarball');
+    cy.contains('button', 'Download')
+      .click();
 
     // Start archive download through the GUI
     cy.get('.modal-dialog')
@@ -370,7 +380,7 @@ describe('Vault Cooking User Interface Tests', function() {
   });
 
   it('should offer to immediately download a revision gitfast archive if already cooked', function() {
-
+    adminLogin();
     // Browse a directory
     cy.visit(this.revisionUrl);
 
@@ -416,7 +426,8 @@ describe('Vault Cooking User Interface Tests', function() {
       response: this.genVaultDirCookingResponse('failed')
     }).as('checkVaultCookingTask');
 
-    checkVaultCookingTask('as tarball');
+    cy.contains('button', 'Download')
+      .click();
 
     // Check that recooking the directory is offered to user
     cy.get('.modal-dialog')

swh/web/browse/snapshot_context.py
@@ -851,7 +851,7 @@ def browse_snapshot_directory(
             "readme_html": readme_html,
             "snapshot_context": snapshot_context,
             "vault_cooking": vault_cooking,
-            "show_actions_menu": True,
+            "show_actions": True,
             "swhids_info": swhids_info,
         },
     )
@@ -1037,7 +1037,7 @@ def browse_snapshot_content(
             "top_right_link": top_right_link,
             "snapshot_context": snapshot_context,
             "vault_cooking": None,
-            "show_actions_menu": True,
+            "show_actions": True,
             "swhids_info": swhids_info,
             "error_code": content_data.get("error_code"),
             "error_message": content_data.get("error_message"),
@@ -1187,7 +1187,7 @@ def browse_snapshot_log(request, snapshot_id=None, origin_url=None, timestamp=No
             "top_right_link": None,
             "snapshot_context": snapshot_context,
             "vault_cooking": None,
-            "show_actions_menu": True,
+            "show_actions": True,
             "swhids_info": swhids_info,
         },
     )
@@ -1436,6 +1436,6 @@ def browse_snapshot_releases(
             "next_releases_url": next_releases_url,
             "snapshot_context": snapshot_context,
             "vault_cooking": None,
-            "show_actions_menu": False,
+            "show_actions": False,
         },
     )

swh/web/browse/views/content.py
@@ -401,7 +401,7 @@ def content_display(request, query_string):
             },
             "snapshot_context": snapshot_context,
             "vault_cooking": None,
-            "show_actions_menu": True,
+            "show_actions": True,
             "swhids_info": swhids_info,
             "error_code": content_data["error_code"],
             "error_message": content_data["error_message"],

swh/web/browse/views/directory.py
@@ -215,7 +215,7 @@ def _directory_browse(request, sha1_git, path=None):
             "readme_html": readme_html,
             "snapshot_context": snapshot_context,
             "vault_cooking": vault_cooking,
-            "show_actions_menu": True,
+            "show_actions": True,
             "swhids_info": swhids_info,
         },
     )

swh/web/browse/views/origin.py
@@ -270,7 +270,7 @@ def _origin_visits_browse(request, origin_url):
             "origin_info": origin_info,
             "snapshot_context": snapshot_context,
             "vault_cooking": None,
-            "show_actions_menu": False,
+            "show_actions": False,
         },
     )
 

swh/web/browse/views/release.py
@@ -232,7 +232,7 @@ def release_browse(request, sha1_git):
             "swh_object_metadata": release_metadata,
             "release": release,
             "snapshot_context": snapshot_context,
-            "show_actions_menu": True,
+            "show_actions": True,
             "breadcrumbs": None,
             "vault_cooking": vault_cooking,
             "top_right_link": None,

commit b913946250542abfb452b0c1a79bbcddd5b75ebb

templates: Fix browsed object metadata availability from javascript
That regression was introduced in 8e0fa87b22d7.

swh/web/templates/includes/show-metadata.html
@@ -7,30 +7,32 @@ See top-level LICENSE file for more information
 
 {% load swh_templatetags %}
 
-<button id="metadata-toggler" class="btn btn-default btn-sm swh-popover-toggler" data-toggle="popover" data-popover-content="#metadata-popover">
-  <i class="mdi mdi-table mdi-fw" aria-hidden="true"></i>
-  Show metadata
-</button>
+{% if user.is_staff %}
+  <button id="metadata-toggler" class="btn btn-default btn-sm swh-popover-toggler" data-toggle="popover" data-popover-content="#metadata-popover">
+    <i class="mdi mdi-table mdi-fw" aria-hidden="true"></i>
+    Show metadata
+  </button>
 
-<div id="metadata-popover" style="display:none;">
-  <div class="popover-heading">{{ swh_object_name }} metadata</div>
-  <div class="popover-body">
-    <table class="table">
-      <tbody>
-        {% for key, val in swh_object_metadata.items|dictsort:"0.lower" %}
-          {% if val is not None and val != '' %}
-            <tr>
-              <th class="swh-metadata-table-row swh-metadata-table-key">{{ key }}</th>
-              <td class="swh-metadata-table-row swh-metadata-table-value">
-                <pre>{{ val | escape }}</pre>
-              </td>
-            </tr>
-          {% endif %}
-        {% endfor %}
-      </tbody>
-    </table>
+  <div id="metadata-popover" style="display:none;">
+    <div class="popover-heading">{{ swh_object_name }} metadata</div>
+    <div class="popover-body">
+      <table class="table">
+        <tbody>
+          {% for key, val in swh_object_metadata.items|dictsort:"0.lower" %}
+            {% if val is not None and val != '' %}
+              <tr>
+                <th class="swh-metadata-table-row swh-metadata-table-key">{{ key }}</th>
+                <td class="swh-metadata-table-row swh-metadata-table-value">
+                  <pre>{{ val | escape }}</pre>
+                </td>
+              </tr>
+            {% endif %}
+          {% endfor %}
+        </tbody>
+      </table>
+    </div>
   </div>
-</div>
+{% endif %}
 
 <script>
   swh.webapp.setBrowsedSwhObjectMetadata({{ swh_object_metadata|jsonify }})

swh/web/templates/includes/top-navigation.html
@@ -111,9 +111,7 @@ See top-level LICENSE file for more information
         {% include "includes/vault-create-tasks.html" %}
       {% endif %}
       {% include "includes/take-new-snapshot.html" %}
-      {% if user.is_staff %}
-        {% include "includes/show-metadata.html" %}
-      {% endif %}
+      {% include "includes/show-metadata.html" %}
     {% endif %}
   </div>
 </div>

cypress/integration/directory.spec.js
@@ -72,4 +72,12 @@ describe('Directory Tests', function() {
     cy.get('.swh-directory-table')
       .should('be.visible');
   });
+
+  it('should have metadata available from javascript', function() {
+    cy.window().then(win => {
+      const metadata = win.swh.webapp.getBrowsedSwhObjectMetadata();
+      expect(metadata).to.not.be.empty;
+      expect(metadata).to.have.any.keys('directory');
+    });
+  });
 });