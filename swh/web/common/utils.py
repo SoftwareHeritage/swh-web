527e7b0a877e71ce9d67f5eb877dabd151f1acb4
Antoine Lambert <antoine.lambert@inria.fr>
2020-11-03 13:06:14 +0100

    templates/profile: Ensure correct base URL is displayed in production

--- swh/web/common/utils.py
+++ swh/web/common/utils.py
@@ -261,6 +261,7 @@ def context_processor(request):
         "oidc_enabled": bool(config["keycloak"]["server_url"]),
         "browsers_supported_image_mimes": browsers_supported_image_mimes,
         "keycloak": config["keycloak"],
+        "site_base_url": request.build_absolute_uri("/"),
     }
 
 
