a3eb1f6c00c871d458916f44fc040110297afac7
Antoine Lambert <antoine.lambert@inria.fr>
2021-03-15 17:25:23 +0100

    settings/production: Disable per-site caching for authenticated users
    Django per-site caching messes up some page rendering for authenticated
    users when used in combination with gunicorn workers and memcached.
    
    So remove per-site caching in production settings as it is not really
    needed plus it is redundant with the varnish cache already deployed
    in front of swh-web in production.

--- swh/web/settings/production.py
+++ swh/web/settings/production.py
@@ -10,15 +10,8 @@ Django production settings for swh-web.
 from .common import *  # noqa
 from .common import CACHES, MIDDLEWARE, REST_FRAMEWORK, WEBPACK_LOADER, swh_web_config
 
-# activate per-site caching
-if "GZip" in MIDDLEWARE[0]:
-    MIDDLEWARE.insert(1, "django.middleware.cache.UpdateCacheMiddleware")
-else:
-    MIDDLEWARE.insert(0, "django.middleware.cache.UpdateCacheMiddleware")
-
 MIDDLEWARE += [
     "swh.web.common.middlewares.HtmlMinifyMiddleware",
-    "django.middleware.cache.FetchFromCacheMiddleware",
 ]
 
 if swh_web_config.get("throttling", {}).get("cache_uri"):
