3c06b0331d4c42512cbf8572c2abcba855752039
Antoine Lambert <anlambert@softwareheritage.org>
2021-07-01 18:04:54 +0200

    assets/diff-utils: Use homemade missing new line icon instead of GH one
    Create a missing new line at end of file icon by combining two Material Design
    ones instead of reusing a SVG copy of the old GitHub one.
    
    This removes a copyright infringement while having a better looking icon.

--- assets/src/bundles/revision/diff-utils.js
+++ assets/src/bundles/revision/diff-utils.js
@@ -18,12 +18,11 @@ let nbChangedFiles = 0;
 // to track the number of already computed files diffs
 let nbDiffsComputed = 0;
 
-// the no newline at end of file marker from Github
-const noNewLineMarker = '<span class="no-nl-marker" title="No newline at end of file">' +
-                        '<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16">' +
-                          '<path fill-rule="evenodd" d="M16 5v3c0 .55-.45 1-1 1h-3v2L9 8l3-3v2h2V5h2zM8 8c0 2.2-1.8 4-4 4s-4-1.8-4-4 1.8-4 4-4 4 1.8 4 4zM1.5 9.66L5.66 5.5C5.18 5.19 4.61 5 4 5 2.34 5 1 6.34 1 8c0 .61.19 1.17.5 1.66zM7 8c0-.61-.19-1.17-.5-1.66L2.34 10.5c.48.31 1.05.5 1.66.5 1.66 0 3-1.34 3-3z"></path>' +
-                        '</svg>' +
-                      '</span>';
+const noNewLineMarker =
+  '<span class="no-nl-marker" style="color: red;" title="No newline at end of file">' +
+    '<i class="mdi mdi-rotate-90 mdi-cancel" aria-hidden="true">' +
+    '<i class="mdi mdi-keyboard-return" aria-hidden="true">' +
+  '</span>';
 
 // to track the total number of added lines in files diffs
 let nbAdditions = 0;
