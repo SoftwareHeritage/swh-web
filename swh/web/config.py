c5d441b1a8ec425b995ac72ab2c76c66a496ca1b
Antoine Lambert <anlambert@softwareheritage.org>
2021-07-19 17:18:31 +0200

    common/utils: Wrap deposits list retrieval in a function
    It will enable to exploit those data in the archive coverage widget
    implementation to get deposit counts by origins type.
    
    Related to T3127

--- swh/web/config.py
+++ swh/web/config.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017-2020  The Software Heritage developers
+# Copyright (C) 2017-2021  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -105,7 +105,7 @@ DEFAULT_CONFIG = {
         {
             "private_api_url": "https://deposit.softwareheritage.org/1/private/",
             "private_api_user": "swhworker",
-            "private_api_password": "",
+            "private_api_password": "some-password",
         },
     ),
     "coverage_count_origins": ("bool", False),
