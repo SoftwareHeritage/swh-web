SoftwareHeritage/swh-web

commit 835a86012630f83bed0e5b849724c67509263d5f

browse/snapshot_context: Fix revision_found setting in directory view
That variable was set in an incorrect way. As revision_id can be None,
it should be initialized to False and set to True after checking the
revision can be found in the archive.

This fixes the non display of the "History" button in the snapshot
directory view when a snapshot contains no revisions.

Related to T3840

swh/web/browse/snapshot_context.py
@@ -823,13 +823,15 @@ def browse_snapshot_directory(
 
     swh_objects = []
     vault_cooking = {}
-    revision_found = True
+    revision_found = False
 
     if sha1_git is None and revision_id is not None:
         try:
             archive.lookup_revision(revision_id)
         except NotFoundExc:
-            revision_found = False
+            pass
+        else:
+            revision_found = True
 
     if sha1_git is not None:
         swh_objects.append(

swh/web/tests/browse/views/test_snapshot.py
@@ -363,9 +363,20 @@ def test_browse_snapshot_log_no_revisions(client, archive_data, directory):
     )
     archive_data.snapshot_add([snapshot])
 
-    url = reverse("browse-snapshot-log", url_args={"snapshot_id": snapshot.id.hex()})
+    snp_url = reverse(
+        "browse-snapshot-directory", url_args={"snapshot_id": snapshot.id.hex()}
+    )
+    log_url = reverse(
+        "browse-snapshot-log", url_args={"snapshot_id": snapshot.id.hex()}
+    )
+
     resp = check_html_get_response(
-        client, url, status_code=404, template_used="error.html"
+        client, snp_url, status_code=200, template_used="browse/directory.html"
+    )
+    assert_not_contains(resp, log_url)
+
+    resp = check_html_get_response(
+        client, log_url, status_code=404, template_used="error.html"
     )
     assert_contains(
         resp,

commit 68ec3d83ab5dc2b5c2ef81591ac31fc68a0bb01d

browse/snapshot_context: Fix revisions log link display regression
Fix regression introduced in 835a86012630: History link was no longer
displayed for all origins in snapshot directory view.

swh/web/browse/snapshot_context.py
@@ -825,7 +825,7 @@ def browse_snapshot_directory(
     vault_cooking = {}
     revision_found = False
 
-    if sha1_git is None and revision_id is not None:
+    if revision_id is not None:
         try:
             archive.lookup_revision(revision_id)
         except NotFoundExc:

swh/web/tests/browse/views/test_snapshot.py
@@ -19,16 +19,19 @@ from swh.model.model import (
     OriginVisit,
     OriginVisitStatus,
     Release,
+    Revision,
+    RevisionType,
     Snapshot,
     SnapshotBranch,
     TargetType,
+    TimestampWithTimezone,
 )
 from swh.storage.utils import now
 from swh.web.browse.snapshot_context import process_snapshot_branches
 from swh.web.common.utils import reverse
 from swh.web.tests.data import random_sha1
 from swh.web.tests.django_asserts import assert_contains, assert_not_contains
-from swh.web.tests.strategies import new_origin, visit_dates
+from swh.web.tests.strategies import new_origin, new_person, new_swh_date, visit_dates
 from swh.web.tests.utils import check_html_get_response
 
 
@@ -383,3 +386,42 @@ def test_browse_snapshot_log_no_revisions(client, archive_data, directory):
         "No revisions history found in the current snapshot context.",
         status_code=404,
     )
+
+
+@given(new_person(), new_swh_date())
+def test_browse_snapshot_log_when_revisions(
+    client, archive_data, directory, person, date
+):
+
+    revision = Revision(
+        directory=hash_to_bytes(directory),
+        author=person,
+        committer=person,
+        message=b"commit message",
+        date=TimestampWithTimezone.from_datetime(date),
+        committer_date=TimestampWithTimezone.from_datetime(date),
+        synthetic=False,
+        type=RevisionType.GIT,
+    )
+    archive_data.revision_add([revision])
+
+    snapshot = Snapshot(
+        branches={
+            b"HEAD": SnapshotBranch(
+                target=revision.id, target_type=TargetType.REVISION
+            ),
+        },
+    )
+    archive_data.snapshot_add([snapshot])
+
+    snp_url = reverse(
+        "browse-snapshot-directory", url_args={"snapshot_id": snapshot.id.hex()}
+    )
+    log_url = reverse(
+        "browse-snapshot-log", url_args={"snapshot_id": snapshot.id.hex()}
+    )
+
+    resp = check_html_get_response(
+        client, snp_url, status_code=200, template_used="browse/directory.html"
+    )
+    assert_contains(resp, log_url)