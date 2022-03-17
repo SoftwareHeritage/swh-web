e9933448be0f6d5eb76fc944dacca30d9fa35447
Jayesh Velayudhan <jayesh@softwareheritage.org>
2022-03-17 11:33:57 +0100

    Create "Add forge now" request
    Added a form to submit an "add forge now" request.
    Left menus changed to include "add forge now" page.
    Possbility to browse existing requests is added
    in a seperate tab in the same view.
    Cypress tests
    
    Related to T3998
    Related to T3989
    Related to T4012
    Related to T3988

---
+++ assets/src/bundles/add_forge/create-request.js
@@ -0,0 +1,92 @@
+/**
+ * Copyright (C) 2022  The Software Heritage developers
+ * See the AUTHORS file at the top-level directory of this distribution
+ * License: GNU Affero General Public License version 3, or any later version
+ * See top-level LICENSE file for more information
+ */
+
+import {handleFetchError, removeUrlFragment, csrfPost} from 'utils/functions';
+
+let requestBrowseTable;
+
+export function onCreateRequestPageLoad() {
+  $(document).ready(() => {
+    $('#requestCreateForm').submit(async function(event) {
+      event.preventDefault();
+      try {
+        const response = await csrfPost($(this).attr('action'),
+                                        {'Content-Type': 'application/x-www-form-urlencoded'},
+                                        $(this).serialize());
+        handleFetchError(response);
+        $('#userMessageDetail').empty();
+        $('#userMessage').text('Your request has been submitted');
+        $('#userMessage').removeClass('badge-danger');
+        $('#userMessage').addClass('badge-success');
+        requestBrowseTable.draw(); // redraw the table to update the list
+      } catch (response) {
+        const responseText = await response.json();
+        $('#userMessageDetail').empty();
+        $('#userMessage').text('Sorry; an error occurred');
+        $('#userMessageDetail').text(responseText.substring(0, 500));
+        $('#userMessage').removeClass('badge-success');
+        $('#userMessage').addClass('badge-danger');
+      }
+    });
+
+    $(window).on('hashchange', () => {
+      if (window.location.hash === '#browse-requests') {
+        $('.nav-tabs a[href="#swh-add-forge-requests-list"]').tab('show');
+      } else {
+        $('.nav-tabs a[href="#swh-add-forge-submit-request"]').tab('show');
+      }
+    });
+
+    $('#swh-add-forge-requests-list-tab').on('shown.bs.tab', () => {
+      window.location.hash = '#browse-requests';
+    });
+
+    $('#swh-add-forge-tab').on('shown.bs.tab', () => {
+      removeUrlFragment();
+    });
+
+    populateRequestBrowseList(); // Load existing requests
+  });
+}
+
+export function populateRequestBrowseList() {
+  requestBrowseTable = $('#add-forge-request-browse')
+    .on('error.dt', (e, settings, techNote, message) => {
+      $('#add-forge-browse-request-error').text(message);
+    })
+    .DataTable({
+      serverSide: true,
+      processing: true,
+      retrieve: true,
+      searching: true,
+      info: false,
+      dom: '<<"d-flex justify-content-between align-items-center"f' +
+        '<"#list-exclude">l>rt<"bottom"ip>>',
+      ajax: {
+        'url': Urls.add_forge_request_list_datatables()
+      },
+      columns: [
+        {
+          data: 'submission_date',
+          name: 'submission_date'
+        },
+        {
+          data: 'forge_type',
+          name: 'forge_type'
+        },
+        {
+          data: 'forge_url',
+          name: 'forge_url'
+        },
+        {
+          data: 'status',
+          name: 'status'
+        }
+      ]
+    });
+  requestBrowseTable.draw();
+}
