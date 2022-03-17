d7bb4cb6048479354d0564a7783d2c11fb1150b3
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2022-03-17 17:41:44 +0100

    Add feature flag to toggle on/off add-forge-now feature
    This also prevents the server code from being served when the feature flag is off.
    
    Related to T4040

--- swh/web/admin/urls.py
+++ swh/web/admin/urls.py
@@ -7,10 +7,13 @@ from django.conf.urls import url
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect
 
-import swh.web.admin.add_forge_now  # noqa
 from swh.web.admin.adminurls import AdminUrls
 import swh.web.admin.deposit  # noqa
 import swh.web.admin.origin_save  # noqa
+from swh.web.config import is_feature_enabled
+
+if is_feature_enabled("add_forge_now"):
+    import swh.web.admin.add_forge_now  # noqa
 
 
 def _admin_default_view(request):
