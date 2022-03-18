2c3dedaf6f65e3e31fd4fa8cfe23c037b8dee4d8
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2022-03-18 11:15:51 +0100

    settings.development: Activate add-forge-now feature for development
    Related to T4040

--- swh/web/settings/development.py
+++ swh/web/settings/development.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017-2019  The Software Heritage developers
+# Copyright (C) 2017-2022  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -9,8 +9,15 @@ Django development settings for swh-web.
 
 from django.core.cache import cache
 
+from swh.web.config import get_config
+
 from .common import *  # noqa
 from .common import MIDDLEWARE
+
+swh_web_config = get_config()
+swh_web_config.update(
+    {"features": {"add_forge_now": True,},}
+)
 
 MIDDLEWARE += ["swh.web.common.middlewares.HtmlPrettifyMiddleware"]
 
