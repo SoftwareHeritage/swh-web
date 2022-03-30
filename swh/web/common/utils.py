96b38d99363341ff8291ca987beaf16409ddaf0e
Antoine Lambert <anlambert@softwareheritage.org>
2022-03-30 18:07:47 +0200

    admin/deposit: Use new deposit private API endpoint to list deposits
    Previously, the admin-deposit-list view was retrieving all deposits data
    using the /1/private/deposits/ swh-deposit endpoint and was filtering
    those client side.
    
    That approach was not efficient at all as the number of deposits
    increased.
    
    So from now on, swh-web will simply forward the listing requests to
    the new /1/private/deposits/datatables/ endpoint of swh-deposit which
    will perform the same filtering and pagination operations as before in
    an efficient way.
    
    Also remove code that is now dead and ensure deposit private API base
    URL can be set in configuration with or without trailing slash.
    
    Related to T3128

--- swh/web/common/utils.py
+++ swh/web/common/utils.py
@@ -9,7 +9,6 @@ import os
 import re
 from typing import Any, Callable, Dict, List, Optional
 import urllib.parse
-from xml.etree import ElementTree
 
 from bs4 import BeautifulSoup
 from docutils.core import publish_parts
@@ -461,7 +460,8 @@ def _deposits_list_url(
 def get_deposits_list(username: Optional[str] = None) -> List[Dict[str, Any]]:
     """Return the list of software deposits using swh-deposit API"""
     config = get_config()["deposit"]
-    deposits_list_base_url = config["private_api_url"] + "deposits"
+    private_api_url = config["private_api_url"].rstrip("/") + "/"
+    deposits_list_base_url = private_api_url + "deposits"
     deposits_list_auth = HTTPBasicAuth(
         config["private_api_user"], config["private_api_password"]
     )
@@ -490,13 +490,6 @@ def get_deposits_list(username: Optional[str] = None) -> List[Dict[str, Any]]:
     return deposits_data["results"]
 
 
-@django_cache()
-def get_deposit_raw_metadata(deposit_id: int) -> Optional[str]:
-    config = get_config()["deposit"]
-    url = f"{config['private_api_url']}/{deposit_id}/meta"
-    return requests.get(url).json()["raw_metadata"]
-
-
 _origin_visit_types_cache_timeout = 24 * 60 * 60  # 24 hours
 
 
@@ -523,73 +516,6 @@ def redirect_to_new_route(request, new_route, permanent=True):
         reverse(new_route, query_params=args),
         permanent=permanent,
     )
-
-
-NAMESPACES = {
-    "swh": "https://www.softwareheritage.org/schema/2018/deposit",
-    "schema": "http://schema.org/",
-}
-
-
-def parse_swh_metadata_provenance(raw_metadata: str) -> Optional[str]:
-    """Parse swh metadata-provenance out of the raw metadata deposit. If found, returns the
-    value, None otherwise.
-
-    .. code-block:: xml
-
-         <swh:deposit>
-           <swh:metadata-provenance>
-             <schema:url>https://example.org/metadata/url</schema:url>
-           </swh:metadata-provenance>
-         </swh:deposit>
-
-    Args:
-        raw_metadata: raw metadata out of deposits received
-
-    Returns:
-        Either the metadata provenance url if any or None otherwise
-
-    """
-    metadata = ElementTree.fromstring(raw_metadata)
-    url = metadata.findtext(
-        "swh:deposit/swh:metadata-provenance/schema:url",
-        namespaces=NAMESPACES,
-    )
-    return url or None
-
-
-def parse_swh_deposit_origin(raw_metadata: str) -> Optional[str]:
-    """Parses <swh:add_to_origin> and <swh:create_origin> from metadata document,
-    if any. They are mutually exclusive and tested as such in the deposit.
-
-    .. code-block:: xml
-
-       <swh:deposit>
-         <swh:create_origin>
-           <swh:origin url='https://example.org/repo/software123/'/>
-         </swh:reference>
-       </swh:deposit>
-
-    .. code-block:: xml
-
-       <swh:deposit>
-         <swh:add_to_origin>
-           <swh:origin url='https://example.org/repo/software123/'/>
-         </swh:add_to_origin>
-       </swh:deposit>
-
-    Returns:
-        The one not null if any, None otherwise
-
-    """
-    metadata = ElementTree.fromstring(raw_metadata)
-    for origin_tag in ["create_origin", "add_to_origin"]:
-        elt = metadata.find(
-            f"swh:deposit/swh:{origin_tag}/swh:origin[@url]", namespaces=NAMESPACES
-        )
-        if elt is not None:
-            return elt.attrib["url"]
-    return None
 
 
 def has_add_forge_now_permission(user) -> bool:
