96fefcf4742b36beb6a68bceb941de2d6ff0b38a
Valentin Lorentz <vlorentz@softwareheritage.org>
2022-07-13 10:29:24 +0200

    Add link to origin extrinsic metadata API endpoint from the UI
    For staff users only, like other extrinsic metadata

--- swh/web/tests/browse/views/test_origin.py
+++ swh/web/tests/browse/views/test_origin.py
@@ -51,7 +51,10 @@ def test_origin_visits_browse(client, archive_data, origin_with_multiple_visits)
     _check_origin_link(resp, origin_url)
 
 
-def test_origin_root_directory_view(client, archive_data, swh_scheduler, origin):
+@pytest.mark.django_db
+def test_origin_root_directory_view(
+    client, staff_user, archive_data, swh_scheduler, origin
+):
     origin_visits = archive_data.origin_visit_get(origin["url"])
 
     visit = origin_visits[-1]
@@ -65,6 +68,7 @@ def test_origin_root_directory_view(client, archive_data, swh_scheduler, origin)
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -77,6 +81,7 @@ def test_origin_root_directory_view(client, archive_data, swh_scheduler, origin)
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -90,6 +95,7 @@ def test_origin_root_directory_view(client, archive_data, swh_scheduler, origin)
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -103,6 +109,7 @@ def test_origin_root_directory_view(client, archive_data, swh_scheduler, origin)
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -116,6 +123,7 @@ def test_origin_root_directory_view(client, archive_data, swh_scheduler, origin)
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -128,6 +136,7 @@ def test_origin_root_directory_view(client, archive_data, swh_scheduler, origin)
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -141,6 +150,7 @@ def test_origin_root_directory_view(client, archive_data, swh_scheduler, origin)
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -154,6 +164,7 @@ def test_origin_root_directory_view(client, archive_data, swh_scheduler, origin)
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -166,7 +177,10 @@ def test_origin_root_directory_view(client, archive_data, swh_scheduler, origin)
     )
 
 
-def test_origin_sub_directory_view(client, archive_data, swh_scheduler, origin):
+@pytest.mark.django_db
+def test_origin_sub_directory_view(
+    client, staff_user, archive_data, swh_scheduler, origin
+):
     origin_visits = archive_data.origin_visit_get(origin["url"])
 
     visit = origin_visits[-1]
@@ -189,6 +203,7 @@ def test_origin_sub_directory_view(client, archive_data, swh_scheduler, origin):
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -202,6 +217,7 @@ def test_origin_sub_directory_view(client, archive_data, swh_scheduler, origin):
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -216,6 +232,7 @@ def test_origin_sub_directory_view(client, archive_data, swh_scheduler, origin):
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -230,6 +247,7 @@ def test_origin_sub_directory_view(client, archive_data, swh_scheduler, origin):
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -244,6 +262,7 @@ def test_origin_sub_directory_view(client, archive_data, swh_scheduler, origin):
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -257,6 +276,7 @@ def test_origin_sub_directory_view(client, archive_data, swh_scheduler, origin):
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -271,6 +291,7 @@ def test_origin_sub_directory_view(client, archive_data, swh_scheduler, origin):
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -285,6 +306,7 @@ def test_origin_sub_directory_view(client, archive_data, swh_scheduler, origin):
 
     _origin_directory_view_test_helper(
         client,
+        staff_user,
         archive_data,
         origin,
         visit,
@@ -774,6 +796,7 @@ def test_origin_content_view_legacy_redirects(client, new_origin):
 
 def _origin_directory_view_test_helper(
     client,
+    staff_user,
     archive_data,
     origin_info,
     origin_visit,
@@ -915,6 +938,43 @@ def _origin_directory_view_test_helper(
     _check_origin_link(resp, origin_info["url"])
 
     assert_not_contains(resp, "swh-metadata-popover")
+
+    # Finally, check "Extrinsic metadata" dropdown:
+
+    origin_metadata_api_url = reverse(
+        "api-1-raw-extrinsic-metadata-origin-authorities",
+        url_args={"origin_url": origin_info["url"]},
+    )
+    directory_metadata_api_url = reverse(
+        "api-1-raw-extrinsic-metadata-swhid-authorities",
+        url_args={"target": f"swh:1:dir:{root_directory_sha1}"},
+    )
+    extrinsic_metadata_snippets = [
+        "Extrinsic metadata",
+        f'<a href="{origin_metadata_api_url}" class="dropdown-item" role="button">',
+        f'<a href="{directory_metadata_api_url}" class="dropdown-item" role="button">',
+    ]
+
+    client.logout()
+
+    resp = check_html_get_response(
+        client, url, status_code=200, template_used="browse/directory.html"
+    )
+    # None of the above should be present for logged-out users
+    for snippet in extrinsic_metadata_snippets:
+        assert_not_contains(resp, snippet)
+
+    client.force_login(staff_user)
+
+    resp = check_html_get_response(
+        client, url, status_code=200, template_used="browse/directory.html"
+    )
+
+    # But they should for staff users
+    for snippet in extrinsic_metadata_snippets:
+        assert_contains(resp, snippet)
+
+    client.logout()
 
 
 def _check_origin_link(resp, origin_url):
