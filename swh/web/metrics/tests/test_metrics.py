b4fc49b3fe92651f950a83705d5d952ec488ad8c
Antoine Lambert <anlambert@softwareheritage.org>
2022-11-28 14:32:22 +0100

    metrics: Add gauge counting save origin requests created from webhooks
    It enables to have some statistics in grafana about the number of save code
    now requests created from forge webhook receivers.
    
    Related to T4548

--- swh/web/metrics/tests/test_metrics.py
+++ swh/web/metrics/tests/test_metrics.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019-2021  The Software Heritage developers
+# Copyright (C) 2019-2022  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -13,6 +13,7 @@ import pytest
 from swh.web.metrics.prometheus import (
     ACCEPTED_SAVE_REQUESTS_DELAY_METRIC,
     ACCEPTED_SAVE_REQUESTS_METRIC,
+    SUBMITTED_SAVE_REQUESTS_FROM_WEBHOOKS_METRIC,
     SUBMITTED_SAVE_REQUESTS_METRIC,
     get_savable_visit_types,
 )
@@ -28,6 +29,7 @@ from swh.web.save_code_now.models import (
     SAVE_TASK_SUCCEEDED,
     SaveOriginRequest,
 )
+from swh.web.save_origin_webhooks.generic_receiver import SUPPORTED_FORGE_TYPES
 from swh.web.tests.django_asserts import assert_contains
 from swh.web.tests.helpers import check_http_get_response
 from swh.web.utils import reverse
@@ -51,16 +53,23 @@ def test_origin_save_metrics(client, swh_scheduler):
         SAVE_TASK_RUNNING,
     )
 
-    for _ in range(random.randint(50, 100)):
+    supported_forge_types = list(SUPPORTED_FORGE_TYPES)
+
+    for i in range(random.randint(50, 100)):
         visit_type = random.choice(visit_types)
         request_satus = random.choice(request_statuses)
         load_task_status = random.choice(load_task_statuses)
+        from_webhook = i % 2 == 0
 
         sor = SaveOriginRequest.objects.create(
             origin_url="origin",
             visit_type=visit_type,
             status=request_satus,
             loading_task_status=load_task_status,
+            from_webhook=from_webhook,
+            webhook_origin=random.choice(supported_forge_types)
+            if from_webhook
+            else None,
         )
 
         if load_task_status in (SAVE_TASK_SUCCEEDED, SAVE_TASK_FAILED):
@@ -102,6 +111,20 @@ def test_origin_save_metrics(client, swh_scheduler):
             f"{SUBMITTED_SAVE_REQUESTS_METRIC}{{"
             f'status="{labels[1]}",'
             f'visit_type="{labels[0]}"}} {float(sor_count)}\n'
+        )
+
+        assert_contains(resp, metric_text)
+
+    labels_set = product(request_statuses, SUPPORTED_FORGE_TYPES)
+    for labels in labels_set:
+        sor_count = SaveOriginRequest.objects.filter(
+            status=labels[0], webhook_origin=labels[1]
+        ).count()
+
+        metric_text = (
+            f"{SUBMITTED_SAVE_REQUESTS_FROM_WEBHOOKS_METRIC}{{"
+            f'status="{labels[0]}",'
+            f'webhook_origin="{labels[1]}"}} {float(sor_count)}\n'
         )
 
         assert_contains(resp, metric_text)
