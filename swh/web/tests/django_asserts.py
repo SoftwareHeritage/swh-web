48c8ce19b3292f8506b9482d41294880cab5d911
Antoine Lambert <anlambert@softwareheritage.org>
2024-06-14 12:09:34 +0200

    tests/django_asserts: Use pytest-django wrappers
    Instead of defining our own django assertion wrappers, prefer to
    use those from pytest-django.

--- swh/web/tests/django_asserts.py
+++ swh/web/tests/django_asserts.py
@@ -1,21 +1,10 @@
-# Copyright (C) 2019  The Software Heritage developers
+# Copyright (C) 2019-2024  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-# https://github.com/pytest-dev/pytest-django/pull/709 proposes a more
-# generic way to expose all asserts but it makes mypy unhappy.
-# So explicitly expose the assertions we need for swh-web tests to
-# avoid mypy errors
+from pytest_django.asserts import assertContains, assertNotContains, assertTemplateUsed
 
-"""
-Expose some Django assertions to be used with pytest
-"""
-
-from django.test import TestCase
-
-_test_case = TestCase("run")
-
-assert_template_used = _test_case.assertTemplateUsed
-assert_contains = _test_case.assertContains
-assert_not_contains = _test_case.assertNotContains
+assert_template_used = assertTemplateUsed
+assert_contains = assertContains
+assert_not_contains = assertNotContains
