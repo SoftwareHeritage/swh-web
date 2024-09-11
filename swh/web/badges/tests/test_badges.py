SoftwareHeritage/swh-web

commit 77dd2a972a0b6dde55fe8f37a49bc2987f406488

utils: Add django URL path converter for SWHID type
It enables to simplify the declaration of a webapp URL containing
a SWHID in its path.

swh/web/badges/urls.py
@@ -1,25 +1,23 @@
-# Copyright (C) 2022  The Software Heritage developers
+# Copyright (C) 2022-2024  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-from django.urls import re_path as url
+from django.urls import path as url
 
 from swh.web.badges import swh_badge, swh_badge_swhid
+from swh.web.utils.url_path_converters import register_url_path_converters
+
+register_url_path_converters()
 
 urlpatterns = [
     url(
-        r"^badge/(?P<object_type>origin)/(?P<object_id>.+)/$",
+        "badge/<str:object_type>/<path:object_id>/",
         swh_badge,
         name="swh-badge",
     ),
     url(
-        r"^badge/(?P<object_type>[a-z]+)/(?P<object_id>[0-9a-f]+)/$",
-        swh_badge,
-        name="swh-badge",
-    ),
-    url(
-        r"^badge/(?P<object_swhid>swh:[0-9]+:[a-z]+:[0-9a-f]+(;.*)?)/$",
+        "badge/<swhid:object_swhid>/",
         swh_badge_swhid,
         name="swh-badge-swhid",
     ),

swh/web/badges/tests/test_badges.py
@@ -4,9 +4,6 @@
 # See top-level LICENSE file for more information
 
 from corsheaders.middleware import ACCESS_CONTROL_ALLOW_ORIGIN
-import pytest
-
-from django.urls.exceptions import NoReverseMatch
 
 from swh.model.hashutil import hash_to_bytes
 from swh.model.swhids import ObjectType, QualifiedSWHID
@@ -88,13 +85,6 @@ def test_badge_errors(
             client, url, status_code=200, content_type="image/svg+xml"
         )
         _check_generated_badge(resp, "", "", error="invalid id")
-
-    for object_type, object_id in (
-        (ObjectType.SNAPSHOT, "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"),
-    ):
-        url_args = {"object_type": object_type.name.lower(), "object_id": object_id}
-        with pytest.raises(NoReverseMatch):
-            reverse("swh-badge", url_args=url_args)
 
 
 def test_badge_endpoints_have_cors_header(client, origin, release):

swh/web/api/urls.py
@@ -3,8 +3,11 @@
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-
 from swh.web.api.apiurls import api_urls
+from swh.web.utils.url_path_converters import register_url_path_converters
+
+register_url_path_converters()
+
 import swh.web.api.views.content  # noqa
 import swh.web.api.views.directory  # noqa
 import swh.web.api.views.extid  # noqa

swh/web/browse/urls.py
@@ -1,11 +1,11 @@
-# Copyright (C) 2017-2022  The Software Heritage developers
+# Copyright (C) 2017-2024  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
-from django.urls import re_path as url
+from django.urls import path as url
 
 from swh.web.browse.browseurls import browse_urls
 from swh.web.browse.identifiers import swhid_browse
@@ -17,6 +17,7 @@ import swh.web.browse.views.release  # noqa
 import swh.web.browse.views.revision  # noqa
 import swh.web.browse.views.snapshot  # noqa
 from swh.web.utils import is_swh_web_production, origin_visit_types, reverse
+from swh.web.utils.url_path_converters import register_url_path_converters
 
 
 def _browse_help_view(request: HttpRequest) -> HttpResponse:
@@ -44,30 +45,32 @@ def _browse_swhid_iframe_legacy(request: HttpRequest, swhid: str) -> HttpRespons
     return redirect(reverse("browse-swhid-iframe", url_args={"swhid": swhid}))
 
 
+register_url_path_converters()
+
 urlpatterns = [
-    url(r"^browse/$", _browse_search_view),
-    url(r"^browse/help/$", _browse_help_view, name="browse-help"),
-    url(r"^browse/search/$", _browse_search_view, name="browse-search"),
+    url("browse/", _browse_search_view),
+    url("browse/help/", _browse_help_view, name="browse-help"),
+    url("browse/search/", _browse_search_view, name="browse-search"),
     # for backward compatibility
-    url(r"^browse/origin/save/$", _browse_origin_save_view, name="browse-origin-save"),
+    url("browse/origin/save/", _browse_origin_save_view, name="browse-origin-save"),
     url(
-        r"^browse/(?P<swhid>swh:[0-9]+:[a-z]+:[0-9a-f]+.*)/$",
+        "browse/<swhid:swhid>/",
         swhid_browse,
         name="browse-swhid-legacy",
     ),
     url(
-        r"^embed/(?P<swhid>swh:[0-9]+:[a-z]+:[0-9a-f]+.*)/$",
+        "embed/<swhid:swhid>/",
         _browse_swhid_iframe_legacy,
         name="browse-swhid-iframe-legacy",
     ),
     # keep legacy SWHID resolving URL with trailing slash for backward compatibility
     url(
-        r"^(?P<swhid>(swh|SWH):[0-9]+:[A-Za-z]+:[0-9A-Fa-f]+.*)/$",
+        "<swhid:swhid>/",
         swhid_browse,
         name="browse-swhid-legacy",
     ),
     url(
-        r"^(?P<swhid>(swh|SWH):[0-9]+:[A-Za-z]+:[0-9A-Fa-f]+.*)$",
+        "<swhid:swhid>",
         swhid_browse,
         name="browse-swhid",
     ),

swh/web/urls.py
@@ -17,6 +17,7 @@ from swh.web.utils.exc import swh_handle400, swh_handle403, swh_handle404, swh_h
 
 swh_web_config = get_config()
 
+
 urlpatterns: List[Union[URLPattern, URLResolver]] = []
 
 # Register URLs for each SWH Django application

swh/web/utils/__init__.py
@@ -42,9 +42,6 @@ from swh.web.auth.utils import (
 from swh.web.config import get_config, search
 from swh.web.utils.exc import BadInputExc, sentry_capture_exception
 
-SWHID_RE = "swh:1:[a-z]{3}:[0-9a-z]{40}"
-
-
 swh_object_icons = {
     "alias": "mdi mdi-star",
     "branch": "mdi mdi-source-branch",

swh/web/api/views/extid.py
@@ -10,12 +10,12 @@ from swh.web.api.apidoc import api_doc, format_docstring
 from swh.web.api.apiurls import api_route
 from swh.web.api.utils import enrich_extid
 from swh.web.api.views.utils import api_lookup
-from swh.web.utils import SWHID_RE, archive
+from swh.web.utils import archive
 from swh.web.utils.exc import BadInputExc
 
 
 @api_route(
-    f"/extid/target/(?P<swhid>{SWHID_RE})/",
+    "/extid/target/<swhid:swhid>/",
     "api-1-extid-target",
 )
 @api_doc("/extid/target/", category="External IDentifiers")

swh/web/api/views/metadata.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021-2022  The Software Heritage developers
+# Copyright (C) 2021-2024  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -17,12 +17,12 @@ from swh.model import hashutil, swhids
 from swh.model.model import MetadataAuthority, MetadataAuthorityType, Origin
 from swh.web.api.apidoc import api_doc, format_docstring
 from swh.web.api.apiurls import api_route
-from swh.web.utils import SWHID_RE, archive, converters, reverse
+from swh.web.utils import archive, converters, reverse
 from swh.web.utils.exc import BadInputExc, NotFoundExc
 
 
 @api_route(
-    f"/raw-extrinsic-metadata/swhid/(?P<target>{SWHID_RE})/",
+    "/raw-extrinsic-metadata/swhid/<swhid:target>/",
     "api-1-raw-extrinsic-metadata-swhid",
 )
 @api_doc("/raw-extrinsic-metadata/swhid/", category="Metadata")
@@ -219,7 +219,7 @@ def api_raw_extrinsic_metadata_get(request: Request, id: str):
 
 
 @api_route(
-    f"/raw-extrinsic-metadata/swhid/(?P<target>{SWHID_RE})/authorities/",
+    "/raw-extrinsic-metadata/swhid/<swhid:target>/authorities/",
     "api-1-raw-extrinsic-metadata-swhid-authorities",
 )
 @api_doc("/raw-extrinsic-metadata/swhid/authorities/", category="Metadata")

commit eaee83b4944e751077d7dfa73da452c93985663e

badges: Fix regression introduced in 77dd2a9
Reorder badges URL patterns as URL pattern taking a SWHID as
argument should be checked prior the one taking object_type
and object_id as arguments (as a qualified SWHID can also match
that URL pattern).

Return more meaningful HTTP status code when an error occurs
when attempting to generate a badge.

swh/web/badges/__init__.py
@@ -97,6 +97,7 @@ def swh_badge(
     """
     left_text = "error"
     whole_link: str | None = None
+    status_code = 200
 
     # cache badge for 30 days by default
     cache_timeout = (
@@ -160,9 +161,11 @@ def swh_badge(
     except (BadInputExc, ValidationError, ValueError):
         right_text = f'invalid {object_type if object_type else "object"} id'
         object_type = "error"
+        status_code = 400
     except NotFoundExc:
         right_text = f'{object_type if object_type else "object"} not found'
         object_type = "error"
+        status_code = 404
 
     badge_data = badge(
         left_text=left_text,
@@ -174,7 +177,9 @@ def swh_badge(
         embed_logo=True,
     )
 
-    response = HttpResponse(badge_data, content_type="image/svg+xml")
+    response = HttpResponse(
+        badge_data, content_type="image/svg+xml", status=status_code
+    )
     return cast(HttpResponse, cachemiddleware.process_response(request, response))
 
 

swh/web/badges/urls.py
@@ -12,13 +12,13 @@ register_url_path_converters()
 
 urlpatterns = [
     url(
-        "badge/<str:object_type>/<path:object_id>/",
-        swh_badge,
-        name="swh-badge",
-    ),
-    url(
         "badge/<swhid:object_swhid>/",
         swh_badge_swhid,
         name="swh-badge-swhid",
+    ),
+    url(
+        "badge/<str:object_type>/<path:object_id>/",
+        swh_badge,
+        name="swh-badge",
     ),
 ]

swh/web/badges/tests/test_badges.py
@@ -59,9 +59,9 @@ def test_badge_errors(
         url_args = {"object_type": object_type, "object_id": object_id}
         url = reverse("swh-badge", url_args=url_args)
         resp = check_http_get_response(
-            client, url, status_code=200, content_type="image/svg+xml"
+            client, url, status_code=404, content_type="image/svg+xml"
         )
-        _check_generated_badge(resp, **url_args, error="not found")
+        _check_generated_badge(resp, **url_args, error="not found", status_code=404)
 
     for object_type, object_id in (
         (ObjectType.CONTENT, invalid_sha1),
@@ -75,16 +75,16 @@ def test_badge_errors(
         url = reverse("swh-badge", url_args=url_args)
 
         resp = check_http_get_response(
-            client, url, status_code=200, content_type="image/svg+xml"
+            client, url, status_code=400, content_type="image/svg+xml"
         )
-        _check_generated_badge(resp, **url_args, error="invalid id")
+        _check_generated_badge(resp, **url_args, error="invalid id", status_code=400)
 
         object_swhid = f"swh:1:{object_type.value}:{object_id}"
         url = reverse("swh-badge-swhid", url_args={"object_swhid": object_swhid})
         resp = check_http_get_response(
-            client, url, status_code=200, content_type="image/svg+xml"
+            client, url, status_code=400, content_type="image/svg+xml"
         )
-        _check_generated_badge(resp, "", "", error="invalid id")
+        _check_generated_badge(resp, "", "", error="invalid id", status_code=400)
 
 
 def test_badge_endpoints_have_cors_header(client, origin, release):
@@ -128,6 +128,7 @@ def _test_badge_endpoints(client, object_type: str, object_id: str):
             QualifiedSWHID(
                 object_type=ObjectType[object_type.upper()],
                 object_id=hash_to_bytes(object_id),
+                origin="https://git.example.org/user/project",
             )
         )
         url = reverse("swh-badge-swhid", url_args={"object_swhid": obj_swhid})
@@ -137,8 +138,9 @@ def _test_badge_endpoints(client, object_type: str, object_id: str):
         _check_generated_badge(resp, **url_args)
 
 
-def _check_generated_badge(response, object_type, object_id, error=None):
-    assert response.status_code == 200, response.content
+def _check_generated_badge(
+    response, object_type, object_id, error=None, status_code=200
+):
     assert response["Content-Type"] == "image/svg+xml"
 
     if not object_type:
@@ -171,10 +173,14 @@ def _check_generated_badge(response, object_type, object_id, error=None):
         object_type = "error"
         assert "Cache-Control" not in response
 
-    assert_contains(response, "<svg ")
-    assert_contains(response, "</svg>")
-    assert_contains(response, get_logo_data())
-    assert_contains(response, badge_config[object_type]["color"])
-    assert_contains(response, badge_config[object_type]["title"])
-    assert_contains(response, text)
-    assert_contains(response, link)
+    assert_contains(response, "<svg ", status_code=status_code)
+    assert_contains(response, "</svg>", status_code=status_code)
+    assert_contains(response, get_logo_data(), status_code=status_code)
+    assert_contains(
+        response, badge_config[object_type]["color"], status_code=status_code
+    )
+    assert_contains(
+        response, badge_config[object_type]["title"], status_code=status_code
+    )
+    assert_contains(response, text, status_code=status_code)
+    assert_contains(response, link, status_code=status_code)