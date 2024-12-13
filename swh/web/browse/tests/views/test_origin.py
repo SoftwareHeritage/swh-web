SoftwareHeritage/swh-web

commit 937a9609471b8de5b54601fb613d74512b716858

browse/snapshot_context: Ensure full VCS visit type is selected by default
When no visit type for an origin is explicitly provided as query parameter,
ensure a full VCS visit type (git, svn or hg) is selected by default when
an origin has multiple visit types.

Related to #4786.

swh/web/browse/snapshot_context.py
@@ -449,7 +449,7 @@ def get_snapshot_context(
     origin_info = None
     visit_info = None
     url_args = {}
-    query_params: Dict[str, Any] = {}
+    query_params: Dict[str, Any] = {"visit_type": visit_type}
     origin_visits_url = None
 
     if origin_url:
@@ -460,9 +460,26 @@ def get_snapshot_context(
 
         origin_info = archive.lookup_origin(origin_url)
 
+        query_params["origin_url"] = origin_info["url"]
+        if len(origin_info["visit_types"]) > 1 and visit_type is None:
+            # avoid selecting git-checkout, svn-export or hg-checkout visit types
+            # when no visit type is explicitly provided
+            for visit_type_to_discard, visit_type_to_fetch in [
+                ("git-checkout", "git"),
+                ("hg-checkout", "hg"),
+                ("svn-export", "svn"),
+            ]:
+                if visit_type_to_discard in origin_info["visit_types"]:
+                    visit_type = visit_type_to_fetch
+                    break
+
         visit_info = get_origin_visit(
             origin_info["url"], timestamp, visit_id, snapshot_id, visit_type=visit_type
         )
+
+        if len(origin_info["visit_types"]) > 1:
+            query_params["visit_type"] = visit_info["type"]
+
         formatted_date = format_utc_iso_date(visit_info["date"])
         visit_info["formatted_date"] = formatted_date
         snapshot_id = visit_info["snapshot"]
@@ -483,17 +500,11 @@ def get_snapshot_context(
             origin_info, timestamp, visit_id, snapshot_id
         )
 
-        query_params["origin_url"] = origin_info["url"]
-        if len(origin_info["visit_types"]) > 1:
-            query_params["visit_type"] = visit_info["type"]
-
         origin_visits_url = reverse(
             "browse-origin-visits",
             query_params={
                 "origin_url": origin_info["url"],
-                "visit_type": (
-                    visit_info["type"] if len(origin_info["visit_types"]) > 1 else None
-                ),
+                "visit_type": query_params["visit_type"],
             },
         )
 

swh/web/browse/tests/views/test_origin.py
@@ -1147,3 +1147,40 @@ def test_citations_ui_activation_for_staff(client, origin, staff_user):
 
     resp = check_html_get_response(client, browse_url, status_code=200)
     assert_contains(resp, '<div id="swh-citations"')
+
+
+def test_origin_browse_multiple_visit_types_git_default(
+    client, origin_with_multiple_visit_types
+):
+    origin_url = origin_with_multiple_visit_types
+    origin_info = archive.lookup_origin(origin_url)
+    visit_types = origin_info["visit_types"]
+    default_visit_type = "git"
+
+    assert len(visit_types) > 1 and default_visit_type in visit_types
+
+    origin_visit = archive.lookup_origin_visit_latest(
+        origin_url, type=default_visit_type
+    )
+    assert origin_visit["type"] == default_visit_type
+    browse_url = reverse(
+        "browse-origin-directory", query_params={"origin_url": origin_url}
+    )
+
+    resp = check_html_get_response(client, browse_url, status_code=200)
+
+    # check select element for visit types
+    assert_not_contains(resp, "select-no-arrows")
+
+    # check default visit type is selected
+    assert_contains(
+        resp,
+        f'<option selected value="{default_visit_type}">{default_visit_type}</option>',
+    )
+
+    # check visit_type query parameter is used in links
+    assert_contains(resp, f"&visit_type={default_visit_type}")
+
+    # check correct visit and snapshot are browsed
+    assert_contains(resp, format_utc_iso_date(origin_visit["date"]))
+    assert_contains(resp, "swh:1:snp:" + origin_visit["snapshot"])

swh/web/browse/tests/test_snapshot_context.py
@@ -105,6 +105,7 @@ def test_get_snapshot_context_no_origin(archive_data, snapshot):
         url_args = {"snapshot_id": snapshot}
 
         query_params = dict(kwargs)
+        query_params["visit_type"] = None
         query_params.pop("snapshot_id")
 
         snapshot_context = get_snapshot_context(**kwargs, browse_context=browse_context)
@@ -212,6 +213,7 @@ def test_get_snapshot_context_with_origin(archive_data, origin_with_multiple_vis
         snapshot_context = get_snapshot_context(**kwargs, browse_context=browse_context)
 
         query_params = dict(kwargs)
+        query_params["visit_type"] = None
 
         branches, releases, _ = get_snapshot_content(snapshot)
         revision_id = None
@@ -307,6 +309,7 @@ def _check_branch_release_revision_parameters(
 
     query_params = dict(kwargs)
     query_params.pop("snapshot_id", None)
+    query_params["visit_type"] = None
 
     expected_branch = dict(base_expected_context)
     expected_branch["branch"] = branch["name"]

commit f4a9501a180bb60a0af0969a33f383715286993e

browse/snapshot_context: Fix snapshot resolution regression
The forcing of visit type introduced in 937a960 should only be
done when no snapshot related info have been passed as parameters
to the get_snapshot_context function.

swh/web/browse/snapshot_context.py
@@ -461,7 +461,13 @@ def get_snapshot_context(
         origin_info = archive.lookup_origin(origin_url)
 
         query_params["origin_url"] = origin_info["url"]
-        if len(origin_info["visit_types"]) > 1 and visit_type is None:
+        if (
+            len(origin_info["visit_types"]) > 1
+            and visit_type is None
+            and snapshot_id is None
+            and timestamp is None
+            and visit_id is None
+        ):
             # avoid selecting git-checkout, svn-export or hg-checkout visit types
             # when no visit type is explicitly provided
             for visit_type_to_discard, visit_type_to_fetch in [

swh/web/browse/tests/views/test_origin.py
@@ -33,6 +33,7 @@ from swh.web.utils import (
 )
 from swh.web.utils.exc import NotFoundExc
 from swh.web.utils.identifiers import gen_swhid
+from swh.web.utils.origin_visits import get_origin_visits
 
 
 def test_origin_visits_browse(client, archive_data, origin_with_multiple_visits):
@@ -1184,3 +1185,17 @@ def test_origin_browse_multiple_visit_types_git_default(
     # check correct visit and snapshot are browsed
     assert_contains(resp, format_utc_iso_date(origin_visit["date"]))
     assert_contains(resp, "swh:1:snp:" + origin_visit["snapshot"])
+
+
+def test_origin_with_multiple_visits_browse_snapshots(
+    client, origin_with_multiple_visit_types
+):
+    """Ensure snapshots of origin with multiple visit types can be successfully browsed."""
+    origin_visits = get_origin_visits(origin_with_multiple_visit_types)
+    for origin_visit in origin_visits:
+        url = reverse(
+            "browse-snapshot-directory",
+            url_args={"snapshot_id": origin_visit["snapshot"]},
+            query_params={"origin_url": origin_with_multiple_visit_types},
+        )
+        check_html_get_response(client, url, status_code=200)