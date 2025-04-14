0a66641507cc56f0a029a8c7c20dd7d9d9dfc783
Antoine Lambert <anlambert@softwareheritage.org>
2025-04-14 14:26:03 +0200

    admin/urls: Remove no longer needed guard when building doc
    See swh/devel/swh-docs@ec926dc31cdef0126fad44f93d558bacdd1388cd.

--- swh/web/admin/urls.py
+++ swh/web/admin/urls.py
@@ -1,9 +1,8 @@
-# Copyright (C) 2024  The Software Heritage developers
+# Copyright (C) 2024-2025  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-import os
 
 from django.conf import settings
 from django.contrib import admin
@@ -11,36 +10,33 @@ from django.urls import path as url
 
 from swh.web.config import get_config, oidc_enabled
 
-# prevent code execution when building documentation to avoid errors
-# with autodoc processing
-if "SWH_DOC_BUILD" not in os.environ:
-    config = get_config()
+config = get_config()
 
-    if oidc_enabled():
-        # use swh-auth views in admin site for login/logout when webapp
-        # uses Keycloak for authentication
-        from swh.auth.django.views import oidc_login, oidc_logout
+if oidc_enabled():
+    # use swh-auth views in admin site for login/logout when webapp
+    # uses Keycloak for authentication
+    from swh.auth.django.views import oidc_login, oidc_logout
 
-        admin.site.login = oidc_login  # type: ignore[assignment]
-        admin.site.logout = oidc_logout  # type: ignore[assignment]
+    admin.site.login = oidc_login  # type: ignore[assignment]
+    admin.site.logout = oidc_logout  # type: ignore[assignment]
 
-    if "swh.web.add_forge_now" in settings.SWH_DJANGO_APPS:
-        # register add forge now request model as manageable by admin site
-        from swh.web.add_forge_now.models import Request
+if "swh.web.add_forge_now" in settings.SWH_DJANGO_APPS:
+    # register add forge now request model as manageable by admin site
+    from swh.web.add_forge_now.models import Request
 
-        if not admin.site.is_registered(Request):
-            admin.site.register(Request)
+    if not admin.site.is_registered(Request):
+        admin.site.register(Request)
 
-    if "swh.web.save_code_now" in settings.SWH_DJANGO_APPS:
-        # register save code now request model as manageable by admin site
-        from swh.web.save_code_now.models import SaveOriginRequest
+if "swh.web.save_code_now" in settings.SWH_DJANGO_APPS:
+    # register save code now request model as manageable by admin site
+    from swh.web.save_code_now.models import SaveOriginRequest
 
-        if not admin.site.is_registered(SaveOriginRequest):
-            admin.site.register(SaveOriginRequest)
+    if not admin.site.is_registered(SaveOriginRequest):
+        admin.site.register(SaveOriginRequest)
 
-    admin.site.site_header = "swh-web management"
-    admin.site.site_title = "Software Heritage Web Application management"
+admin.site.site_header = "swh-web management"
+admin.site.site_title = "Software Heritage Web Application management"
 
-    urlpatterns = [
-        url("manage/", admin.site.urls),
-    ]
+urlpatterns = [
+    url("manage/", admin.site.urls),
+]
