210a06d4f48777aa4649e2868aecc0abcad1cd96
Antoine Lambert <anlambert@softwareheritage.org>
2025-04-16 15:23:18 +0200

    utils: Parametrize highlightjs tests

--- swh/web/utils/tests/test_highlightjs.py
+++ swh/web/utils/tests/test_highlightjs.py
@@ -1,55 +1,39 @@
-# Copyright (C) 2017-2022  The Software Heritage developers
+# Copyright (C) 2017-2025  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+import pytest
 
 from swh.web.utils import highlightjs
 
 
-def test_get_hljs_language_from_mime_type():
-    lang = highlightjs.get_hljs_language_from_mime_type("text/plain")
-    assert lang == "plaintext"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/x-c")
-    assert lang == "c"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/x-c++")
-    assert lang == "cpp"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/x-perl")
-    assert lang == "perl"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/x-python")
-    assert lang == "python"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/x-msdos-batch")
-    assert lang == "dos"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/x-tex")
-    assert lang == "latex"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/x-lisp")
-    assert lang == "lisp"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/x-java")
-    assert lang == "java"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/x-makefile")
-    assert lang == "makefile"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/x-shellscript")
-    assert lang == "bash"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("text/html")
-    assert lang == "xml"
-
-    lang = highlightjs.get_hljs_language_from_mime_type("image/png")
-    assert lang is None
+@pytest.mark.parametrize(
+    "mime_type, language",
+    [
+        ("text/plain", "plaintext"),
+        ("text/x-c", "c"),
+        ("text/x-c++", "cpp"),
+        ("text/x-perl", "perl"),
+        ("text/x-python", "python"),
+        ("text/x-msdos-batch", "dos"),
+        ("text/x-tex", "latex"),
+        ("text/x-lisp", "lisp"),
+        ("text/x-java", "java"),
+        ("text/x-makefile", "makefile"),
+        ("text/x-shellscript", "bash"),
+        ("text/html", "xml"),
+        ("image/png", None),
+    ],
+)
+def test_get_hljs_language_from_mime_type(mime_type, language):
+    lang = highlightjs.get_hljs_language_from_mime_type(mime_type)
+    assert lang == language
 
 
-def test_get_hljs_language_from_filename():
-    for filename, language in (
+@pytest.mark.parametrize(
+    "filename, language",
+    [
         ("foo", None),
         ("foo.h", "c"),
         ("foo.c", "c"),
@@ -76,10 +60,14 @@ def test_get_hljs_language_from_filename():
         ("Makefile", "makefile"),
         ("CMakeLists.txt", "cmake"),
         ("robots.txt", "robots-txt"),
-    ):
-        lang = highlightjs.get_hljs_language_from_filename(filename)
-        assert lang == language
+    ],
+)
+def test_get_hljs_language_from_filename(filename, language):
+    lang = highlightjs.get_hljs_language_from_filename(filename)
+    assert lang == language
 
+
+def test_hljs_languages_aliases():
     for alias, language in highlightjs._hljs_languages_aliases().items():
         lang = highlightjs.get_hljs_language_from_filename(f"foo.{alias}")
         assert lang == language
