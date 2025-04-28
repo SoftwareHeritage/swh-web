56b0bbc6b5eda41d59b0f81bf7b1d736dd56bbc8
Antoine Lambert <anlambert@softwareheritage.org>
2025-04-28 16:24:32 +0200

    utils/exc: Fix mypy warnings since latest django-stubs release

--- swh/web/utils/exc.py
+++ swh/web/utils/exc.py
@@ -1,19 +1,26 @@
-# Copyright (C) 2015-2024  The Software Heritage developers
+# Copyright (C) 2015-2025  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
 import logging
 import traceback
-from typing import Optional
+from typing import Optional, TypeVar
 
 from django_ratelimit.exceptions import Ratelimited
 import sentry_sdk
 
 from django.core import exceptions
 from django.http import HttpRequest, HttpResponse
-from django.http.response import JsonResponse
+from django.http.response import (
+    HttpResponseBadRequest,
+    HttpResponseForbidden,
+    HttpResponseNotFound,
+    HttpResponseServerError,
+    JsonResponse,
+)
 from django.shortcuts import render
+from django.template import loader
 from django.utils.html import escape, format_html
 from rest_framework.exceptions import APIException
 from rest_framework.renderers import JSONRenderer
@@ -88,10 +95,15 @@ http_status_code_message = {
     503: "Service unavailable",
 }
 
+HttpResponseType = TypeVar("HttpResponseType", bound=HttpResponse)
+
 
 def _generate_error_page(
-    request: HttpRequest, error_code: int, error_description: str
-) -> HttpResponse:
+    request: HttpRequest,
+    error_description: str,
+    http_response: type[HttpResponseType],
+) -> HttpResponseType:
+    error_code = http_response.status_code
     error_data = {
         "error": http_status_code_message[error_code],
         "reason": error_description,
@@ -101,27 +113,27 @@ def _generate_error_page(
 
     # ensure django error is returned using same content type as Accept request header
     if accepted_media_type in ("application/json", "*/*"):
-        return HttpResponse(
+        return http_response(
             JSONRenderer().render(error_data),
             content_type="application/json",
-            status=error_code,
         )
     elif accepted_media_type == "application/yaml":
-        return HttpResponse(
+        return http_response(
             YAMLRenderer().render(error_data),
             content_type="application/yaml",
-            status=error_code,
         )
     else:
-        return render(
-            request,
-            "error.html",
-            {
-                "error_code": error_code,
-                "error_message": http_status_code_message[error_code],
-                "error_description": error_description,
-            },
-            status=error_code,
+        return http_response(
+            loader.render_to_string(
+                template_name="error.html",
+                context={
+                    "error_code": error_code,
+                    "error_message": http_status_code_message[error_code],
+                    "error_description": error_description,
+                },
+                request=request,
+            ),
+            content_type="text/html",
         )
 
 
@@ -181,8 +193,10 @@ def _generate_masked_object_page(
 
 
 def swh_handle400(
-    request: HttpRequest, exception: Optional[Exception] = None
-) -> HttpResponse:
+    request: HttpRequest,
+    exception: Optional[Exception] = None,
+    template_name: str = "",
+) -> HttpResponseBadRequest:
     """
     Custom Django HTTP error 400 handler for swh-web.
     """
@@ -191,30 +205,41 @@ def swh_handle400(
         "something that is perceived to be a client error."
         % escape(request.META["PATH_INFO"])
     )
-    return _generate_error_page(request, 400, error_description)
+    return _generate_error_page(request, error_description, HttpResponseBadRequest)
 
 
-def swh_handle403(request, exception: Optional[Exception] = None) -> HttpResponse:
+def swh_handle403(
+    request,
+    exception: Optional[Exception] = None,
+    template_name: str = "",
+) -> HttpResponseForbidden:
     """
     Custom Django HTTP error 403 handler for swh-web.
     """
     error_description = "The resource %s requires an authentication." % escape(
         request.META["PATH_INFO"]
     )
-    return _generate_error_page(request, 403, error_description)
+    return _generate_error_page(request, error_description, HttpResponseForbidden)
 
 
-def swh_handle404(request, exception: Optional[Exception] = None) -> HttpResponse:
+def swh_handle404(
+    request,
+    exception: Optional[Exception] = None,
+    template_name: str = "",
+) -> HttpResponseNotFound:
     """
     Custom Django HTTP error 404 handler for swh-web.
     """
     error_description = "The resource %s could not be found on the server." % escape(
         request.META["PATH_INFO"]
     )
-    return _generate_error_page(request, 404, error_description)
+    return _generate_error_page(request, error_description, HttpResponseNotFound)
 
 
-def swh_handle500(request: HttpRequest) -> HttpResponse:
+def swh_handle500(
+    request: HttpRequest,
+    template_name: str = "",
+) -> HttpResponseServerError:
     """
     Custom Django HTTP error 500 handler for swh-web.
     """
@@ -222,7 +247,7 @@ def swh_handle500(request: HttpRequest) -> HttpResponse:
         "An unexpected condition was encountered when "
         "requesting resource %s." % escape(request.META["PATH_INFO"])
     )
-    return _generate_error_page(request, 500, error_description)
+    return _generate_error_page(request, error_description, HttpResponseServerError)
 
 
 def sentry_capture_exception(exc: Exception) -> None:
@@ -250,6 +275,14 @@ def sentry_capture_exception(exc: Exception) -> None:
         sentry_sdk.capture_exception(exc)
 
 
+class HttpResponseUnauthorized(HttpResponse):
+    status_code = 401
+
+
+class HttpResponseTooManyRequests(HttpResponse):
+    status_code = 429
+
+
 def handle_view_exception(request: HttpRequest, exc: Exception) -> HttpResponse:
     """
     Function used to generate an error page when an exception
@@ -263,21 +296,21 @@ def handle_view_exception(request: HttpRequest, exc: Exception) -> HttpResponse:
         logger.error(traceback_str)
         error_description = escape(traceback_str)
 
-    error_code = 500
+    http_response: type[HttpResponse] = HttpResponseServerError
     if isinstance(exc, BadInputExc):
-        error_code = 400
+        http_response = HttpResponseBadRequest
     elif isinstance(exc, UnauthorizedExc):
-        error_code = 401
+        http_response = HttpResponseUnauthorized
     elif isinstance(exc, ForbiddenExc):
-        error_code = 403
+        http_response = HttpResponseForbidden
     elif isinstance(exc, NotFoundExc):
-        error_code = 404
+        http_response = HttpResponseNotFound
     elif isinstance(exc, Ratelimited):
-        error_code = 429
+        http_response = HttpResponseTooManyRequests
     elif isinstance(exc, MaskedObjectException):
         return _generate_masked_object_page(request, exc)
 
-    resp = _generate_error_page(request, error_code, error_description)
+    resp = _generate_error_page(request, error_description, http_response)
     if get_config()["debug"]:
         resp.traceback = error_description  # type: ignore[attr-defined]
     return resp
