1279eccc935f7d6973ce591fea4ba8d5795704bb
Antoine Lambert <anlambert@softwareheritage.org>
2026-01-23 17:32:30 +0100

    add_forge_now: Add some missing license headers

--- swh/web/add_forge_now/utils.py
+++ swh/web/add_forge_now/utils.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2023-2025  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU Affero General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
 from urllib.parse import urlparse
 
 from requests import post
