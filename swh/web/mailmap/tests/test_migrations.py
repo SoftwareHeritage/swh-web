0138e9a91686ffeb9cd87988f0e8fb63a89deb36
Antoine Lambert <anlambert@softwareheritage.org>
2026-01-23 17:17:31 +0100

    Fix format of some license headers in source files
    There must be two spaces between the copyrigh years and the authors.
    
    This is to ensure the copyright update pre-commit hook will not
    detect those files as unlicensed.

--- swh/web/mailmap/tests/test_migrations.py
+++ swh/web/mailmap/tests/test_migrations.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022 The Software Heritage developers
+# Copyright (C) 2022  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
--- swh/web/save_code_now/tests/test_migrations.py
+++ swh/web/save_code_now/tests/test_migrations.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021-2024 The Software Heritage developers
+# Copyright (C) 2021-2024  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
