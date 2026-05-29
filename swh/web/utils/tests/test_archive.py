9a1289a48e9f8ab3f9dfa7d9bb6d346858699fed
Antoine Lambert <anlambert@softwareheritage.org>
2026-05-29 17:12:48 +0200

    utils/tests/test_archive: Fix flaky test

--- swh/web/utils/tests/test_archive.py
+++ swh/web/utils/tests/test_archive.py
@@ -789,7 +789,10 @@ def test_lookup_directory_through_revision_ko_not_found(revision):
         )
 
 
-def test_lookup_directory_through_revision_ok(archive_data, revision):
+def test_lookup_directory_through_revision_ok(
+    archive_data, revision_with_files_in_target_directory
+):
+    revision = revision_with_files_in_target_directory
     rev_data = archive_data.revision_get(revision)
     dir_entries = [
         e
