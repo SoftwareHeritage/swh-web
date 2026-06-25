cb2f137229746917eef6b24496183ffc034d6427
Paul Wise <pabs3@bonedaddy.net>
2026-06-25 20:10:33 +0800

    coverage: make the collapsed sections searchable
    bootstrap does not yet support this but onbeforematch allows a workaround.
    
    An alternative would be the browser-native <details> <summary>,
    but bootstrap does not yet support styling this properly,
    so things like the bootstrap transitions are lost.
    
    See-also: https://github.com/twbs/bootstrap/issues/41479
    See-also: https://codepen.io/wdzajicek/pen/VYZawjX

--- swh/web/archive_coverage/assets/index.js
+++ swh/web/archive_coverage/assets/index.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2022-2024  The Software Heritage developers
+ * Copyright (C) 2022-2026  The Software Heritage developers
  * See the AUTHORS file at the top-level directory of this distribution
  * License: GNU Affero General Public License version 3, or any later version
  * See top-level LICENSE file for more information
@@ -8,3 +8,43 @@
 import '@iframe-resizer/child';
 import 'webapp/assets/webapp/webapp.css';
 import './coverage.css';
+
+// FIXME: remove this when merged into bootstrap:
+// https://github.com/twbs/bootstrap/issues/41479
+// https://codepen.io/wdzajicek/pen/VYZawjX
+
+const enableCollapseFind = () => {
+  if (!('onbeforematch' in document.body)) {
+    return;
+  }
+  Array.from(document.querySelectorAll('.collapse')).forEach((item) => {
+    const showHideCollapses = (ev) => {
+      const focus = Boolean(document.querySelector('.swh-coverage-focus'));
+      Array.from(document.querySelectorAll('.collapse')).forEach((item) => {
+        if (item === ev.target) {
+          $(item).collapse('show');
+        } else if (!focus) {
+          $(item).collapse('hide');
+        }
+      });
+    };
+    const onhide = () => {
+      item.hidden = 'until-found';
+      item.role = 'region';
+    };
+    const onshow = () => {
+      item.removeAttribute('hidden');
+      item.removeAttribute('role');
+    };
+    if (item.classList.contains('show')) {
+      onshow();
+    } else {
+      onhide();
+    }
+    item.onbeforematch = showHideCollapses;
+    item.addEventListener('show.bs.collapse', onshow);
+    item.addEventListener('hidden.bs.collapse', onhide);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', enableCollapseFind);
