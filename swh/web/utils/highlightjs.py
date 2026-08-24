7822ee8c3056207f68d636af0e0534a179749d63
Antoine Lambert <anlambert@softwareheritage.org>
2026-08-24 17:09:41 +0200

    highlightjs: Add new languages to highlight

--- swh/web/utils/highlightjs.py
+++ swh/web/utils/highlightjs.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017-2025  The Software Heritage developers
+# Copyright (C) 2017-2026  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU Affero General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -63,6 +63,7 @@ def _hljs_languages_aliases():
         "ak": "aiken",
         "ipynb": "json",
         "typ": "typst",
+        "mlw": "why3",
     }
 
 
